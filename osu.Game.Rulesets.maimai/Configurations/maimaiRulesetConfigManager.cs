8d367890f0c72f8b84404902cedf08b40cdfe7ae
derrick.timmermans@outlook.com <derrick.timmermans@outlook.com>
2020-03-09 14:42:52 +0800

    Add config manager

---
+++ osu.Game.Rulesets.maimai/Configurations/maimaiRulesetConfigManager.cs
@@ -0,0 +1,25 @@
+﻿using osu.Game.Configuration;
+using osu.Game.Rulesets.Configuration;
+
+namespace osu.Game.Rulesets.maimai.Configuration
+{
+    public class maimaiRulesetConfigManager : RulesetConfigManager<maimaiRulesetSettings>
+    {
+        public maimaiRulesetConfigManager(SettingsStore settings, RulesetInfo ruleset, int? variant = null)
+            : base(settings, ruleset, variant)
+        {
+        }
+
+        protected override void InitialiseDefaults()
+        {
+            base.InitialiseDefaults();
+
+            Set(maimaiRulesetSettings.ShowVisualizer, true);
+        }
+    }
+
+    public enum maimaiRulesetSettings
+    {
+        ShowVisualizer
+    }
+}
