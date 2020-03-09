da3550d39f9a0a22bc7b55e971686815c0bd5c57
derrick.timmermans@outlook.com <derrick.timmermans@outlook.com>
2020-03-09 14:42:35 +0800

    Add helper function to obtain angle from position

---
+++ osu.Game.Rulesets.maimai/Utils/DegreesFromPosition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using osuTK;
+
+namespace osu.Game.Rulesets.maimai
+{
+    public static class Utils
+    {
+        public static float GetDegreesFromPosition(this Vector2 target, Vector2 self)
+        {
+            Vector2 offset = self - target;
+            float degrees = (float)MathHelper.RadiansToDegrees(Math.Atan2(-offset.X, offset.Y)) + 24.3f;
+
+            return degrees;
+        }
+    }
+}
