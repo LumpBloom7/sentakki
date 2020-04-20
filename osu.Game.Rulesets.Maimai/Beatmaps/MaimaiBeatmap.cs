42059006a0569b861816b9605f59d38798407551
Coppertine <37494321+Coppertine@users.noreply.github.com>
2020-04-20 23:36:34 +1000

    Complete BeatmapStatistics in MaimaiBeatmap

--- osu.Game.Rulesets.Maimai/Beatmaps/MaimaiBeatmap.cs
+++ osu.Game.Rulesets.Maimai/Beatmaps/MaimaiBeatmap.cs
@@ -9,14 +9,28 @@ namespace osu.Game.Rulesets.Maimai.Beatmaps
     {
         public override IEnumerable<BeatmapStatistic> GetStatistics()
         {
-            int beats = HitObjects.Count;
+            int beats = HitObjects.Count(b => b is Tap);
+            int holds = HitObjects.Count(h => h is Hold);
+            int centreHolds = HitObjects.Count(h => is TouchHold)
 
             return new[]
             {
                 new BeatmapStatistic
                 {
-                    Name = "Hit objects",
+                    Name = "Tap Count",
                     Content = beats.ToString(),
+                    Icon = FontAwesome.Solid.Circle
+                },
+                new BeatmapStatistic
+                {
+                    Name = "Hold Count",
+                    Content = holds.ToString(),
+                    Icon = FontAwesome.Solid.Circle
+                },
+                new BeatmapStatistic
+                {
+                    Name = "Spinner Hold Count",
+                    Content = centreHolds.ToString(),
                     Icon = FontAwesome.Solid.Circle
                 }
             };
