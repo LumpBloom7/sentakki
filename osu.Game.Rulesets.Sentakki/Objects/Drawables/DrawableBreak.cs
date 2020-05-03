bf9378ce63849a0a2d60b4b6483bb6734deedc93
Derrick Timmermans <derrick.timmermans@outlook.com>
2020-05-04 00:24:00 +0800

    Add positional effects to break sample

--- osu.Game.Rulesets.Sentakki/Objects/Drawables/DrawableBreak.cs
+++ osu.Game.Rulesets.Sentakki/Objects/Drawables/DrawableBreak.cs
@@ -1,47 +1,46 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Game.Rulesets.Scoring;
-using System.Diagnostics;
 using osu.Game.Skinning;
 using osu.Game.Audio;
+using osu.Game.Configuration;
+using osu.Framework.Audio;
+using osu.Framework.Bindables;
 
 namespace osu.Game.Rulesets.Sentakki.Objects.Drawables
 {
     public class DrawableBreak : DrawableTap
     {
+        private readonly Bindable<bool> userPositionalHitSounds = new Bindable<bool>(false);
+        private readonly BindableDouble balanceAdjust = new BindableDouble();
         private readonly SkinnableSound breakSound;
         public DrawableBreak(SentakkiHitObject hitObject) : base(hitObject)
         {
             AddRangeInternal(new Drawable[]{
                 breakSound = new SkinnableSound(new SampleInfo("Break"))
             });
+            breakSound.AddAdjustment(AdjustableProperty.Balance, balanceAdjust);
         }
 
-        protected override void CheckForResult(bool userTriggered, double timeOffset)
+        [BackgroundDependencyLoader]
+        private void load(OsuConfigManager osuConfig)
         {
-            Debug.Assert(HitObject.HitWindows != null);
-
-            if (!userTriggered)
-            {
-                if (!HitObject.HitWindows.CanBeHit(timeOffset))
-                {
-                    ApplyResult(r => r.Type = HitResult.Miss);
-                }
-                return;
-            }
-
-            if (HitObject.HitWindows.ResultFor(timeOffset) == HitResult.Miss && Time.Current < HitObject.StartTime) return;
-
-            var result = HitObject.HitWindows.ResultFor(timeOffset);
-            if (result == HitResult.None)
-            {
-                return;
-            }
-            ApplyResult(r => r.Type = result);
-            if (result == HitResult.Perfect)
-                breakSound?.Play();
+            osuConfig.BindWith(OsuSetting.PositionalHitSounds, userPositionalHitSounds);
         }
+
+        public override void PlaySamples()
+        {
+            base.PlaySamples();
+            if (Result.Type == HitResult.Perfect)
+            {
+                const float balance_adjust_amount = 0.4f;
+                balanceAdjust.Value = balance_adjust_amount * (userPositionalHitSounds.Value ? SamplePlaybackPosition - 0.5f : 0);
+                breakSound?.Play();
+            }
+        }
+
     }
 }
