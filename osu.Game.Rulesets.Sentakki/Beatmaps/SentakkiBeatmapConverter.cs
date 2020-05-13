e6bf354563bc226f3986aff4f1fd04e6c0557cc1
Derrick Timmermans <derrick.timmermans@outlook.com>
2020-05-13 20:08:19 +0800

    Massive readability cleanup

--- osu.Game.Rulesets.Sentakki/Beatmaps/SentakkiBeatmapConverter.cs
+++ osu.Game.Rulesets.Sentakki/Beatmaps/SentakkiBeatmapConverter.cs
@@ -1,19 +1,14 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Sentakki.Objects;
-using osu.Game.Rulesets.Sentakki.UI;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Objects.Types;
 using osuTK;
-using osuTK.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using osu.Game.Audio;
-using osu.Game.Beatmaps.ControlPoints;
 
 namespace osu.Game.Rulesets.Sentakki.Beatmaps
 {
@@ -24,7 +19,7 @@ namespace osu.Game.Rulesets.Sentakki.Beatmaps
         public override bool CanConvert() => Beatmap.HitObjects.All(h => h is IHasPosition);
         public bool Experimental = false;
 
-        private Random random;
+        private readonly Random random;
 
         public SentakkiBeatmapConverter(IBeatmap beatmap, Ruleset ruleset)
             : base(beatmap, ruleset)
@@ -40,102 +35,24 @@ namespace osu.Game.Rulesets.Sentakki.Beatmaps
             Vector2 newPos = (original as IHasPosition)?.Position ?? Vector2.Zero;
             newPos.Y = 384 - newPos.Y;
 
-            float angle = newPos.GetDegreesFromPosition(CENTRE_POINT).GetNotePathFromDegrees();
-
-            bool twinNote = original.Samples.Any(s => s.Name == HitSampleInfo.HIT_CLAP);
+            int path = newPos.GetDegreesFromPosition(CENTRE_POINT).GetNotePathFromDegrees();
             List<SentakkiHitObject> objects = new List<SentakkiHitObject>();
 
             switch (original)
             {
-                case IHasCurve curveData:
-                    twinNote = curveData.NodeSamples.Any(s => s.Any(s => s.Name == HitSampleInfo.HIT_CLAP));
-                    objects.Add(new Hold
-                    {
-                        NoteColor = Color4.Crimson,
-                        Angle = angle,
-                        NodeSamples = curveData.NodeSamples,
-                        StartTime = original.StartTime,
-                        EndTime = original.GetEndTime(),
-                        EndPosition = SentakkiExtensions.GetPosition(SentakkiPlayfield.INTERSECTDISTANCE, angle),
-                        Position = SentakkiExtensions.GetPosition(SentakkiPlayfield.NOTESTARTDISTANCE, angle),
-                    });
-                    if (twinNote && Experimental)
-                    {
-                        var taps = SentakkiExtensions.CreateTapFromTicks(original, beatmap, curveData, random);
-                        if (!taps.Any())
-                            objects.Add(new Hold
-                            {
-                                NoteColor = Color4.Crimson,
-                                Angle = angle = (newPos.GetDegreesFromPosition(CENTRE_POINT) + (22.5f * random.Next(1, 6))).GetNotePathFromDegrees(),
-                                NodeSamples = curveData.NodeSamples,
-                                StartTime = original.StartTime,
-                                EndTime = original.GetEndTime(),
-                                EndPosition = SentakkiExtensions.GetPosition(SentakkiPlayfield.INTERSECTDISTANCE, angle),
-                                Position = SentakkiExtensions.GetPosition(SentakkiPlayfield.NOTESTARTDISTANCE, angle),
-                            });
-                        else
-                            objects.AddRange(taps);
-                    }
+                case IHasCurve _:
+                    objects.AddRange(Conversions.CreateHoldNote(original, path, beatmap, random, Experimental));
                     break;
 
-                case IHasEndTime endTimeData:
-                    objects.Add(new TouchHold
-                    {
-                        Position = Vector2.Zero,
-                        StartTime = original.StartTime,
-                        EndTime = endTimeData.EndTime,
-                        Samples = original.Samples,
-                    });
+                case IHasEndTime _:
+                    objects.Add(Conversions.CreateTouchHold(original));
                     break;
 
                 default:
-                    bool strong = original.Samples.Any(s => s.Name == HitSampleInfo.HIT_FINISH);
-                    if (strong)
-                    {
-                        objects.Add(new Break
-                        {
-                            NoteColor = Color4.OrangeRed,
-                            Angle = angle,
-                            Samples = original.Samples,
-                            StartTime = original.StartTime,
-                            EndPosition = SentakkiExtensions.GetPosition(SentakkiPlayfield.INTERSECTDISTANCE, angle),
-                            Position = SentakkiExtensions.GetPosition(SentakkiPlayfield.NOTESTARTDISTANCE, angle),
-                        });
-                        if (twinNote && Experimental)
-                            objects.Add(new Break
-                            {
-                                NoteColor = Color4.OrangeRed,
-                                Angle = angle = (newPos.GetDegreesFromPosition(CENTRE_POINT) + (22.5f * random.Next(1, 6))).GetNotePathFromDegrees(),
-                                Samples = original.Samples,
-                                StartTime = original.StartTime,
-                                EndPosition = SentakkiExtensions.GetPosition(SentakkiPlayfield.INTERSECTDISTANCE, angle),
-                                Position = SentakkiExtensions.GetPosition(SentakkiPlayfield.NOTESTARTDISTANCE, angle),
-                            });
-                    }
-                    else
-                    {
-                        objects.Add(new Tap
-                        {
-                            NoteColor = Color4.Orange,
-                            Angle = angle,
-                            Samples = original.Samples,
-                            StartTime = original.StartTime,
-                            EndPosition = SentakkiExtensions.GetPosition(SentakkiPlayfield.INTERSECTDISTANCE, angle),
-                            Position = SentakkiExtensions.GetPosition(SentakkiPlayfield.NOTESTARTDISTANCE, angle),
-                        });
-                        if (twinNote && Experimental)
-                            objects.Add(new Tap
-                            {
-                                NoteColor = Color4.Orange,
-                                Angle = angle = (newPos.GetDegreesFromPosition(CENTRE_POINT) + (22.5f * random.Next(1, 6))).GetNotePathFromDegrees(),
-                                Samples = original.Samples,
-                                StartTime = original.StartTime,
-                                EndPosition = SentakkiExtensions.GetPosition(SentakkiPlayfield.INTERSECTDISTANCE, angle),
-                                Position = SentakkiExtensions.GetPosition(SentakkiPlayfield.NOTESTARTDISTANCE, angle),
-                            });
-                    }
+                    objects.AddRange(Conversions.CreateTapNote(original, path, random, Experimental));
                     break;
             }
+
             foreach (var hitObject in objects)
                 yield return hitObject;
 
--- osu.Game.Rulesets.Sentakki/SentakkiExtensions.cs
+++ osu.Game.Rulesets.Sentakki/SentakkiExtensions.cs
@@ -1,33 +1,20 @@
-﻿
-using osu.Game.Rulesets.Sentakki.UI;
+﻿using osu.Game.Rulesets.Sentakki.UI;
 using osuTK;
 using System;
-using osu.Framework.Graphics;
-using osu.Game.Rulesets.Sentakki.Objects;
-using System.Collections.Generic;
-using osu.Game.Rulesets.Objects;
-using osu.Game.Rulesets.Objects.Types;
-using osu.Game.Audio;
-using osu.Game.Beatmaps;
-using osu.Game.Beatmaps.ControlPoints;
-using osuTK.Graphics;
-using System.Linq;
 
 namespace osu.Game.Rulesets.Sentakki
 {
     public static class SentakkiExtensions
     {
-        public static Vector2 GetPosition(float distance, float angle)
+        public static float GetAngleFromPath(this int path) => SentakkiPlayfield.PATHANGLES[path];
+        public static Vector2 GetPosition(float distance, int path)
         {
-            return new Vector2(-(distance * (float)Math.Cos((angle + 90f) * (float)(Math.PI / 180))), -(distance * (float)Math.Sin((angle + 90f) * (float)(Math.PI / 180))));
+            return new Vector2(-(distance * (float)Math.Cos((path.GetAngleFromPath() + 90f) * (float)(Math.PI / 180))), -(distance * (float)Math.Sin((path.GetAngleFromPath() + 90f) * (float)(Math.PI / 180))));
         }
 
-        public static float GetDegreesFromPosition(this Vector2 target, Vector2 self)
-        {
-            return (float)MathHelper.RadiansToDegrees(Math.Atan2(target.X - self.X, target.Y - self.Y));
-        }
+        public static float GetDegreesFromPosition(this Vector2 target, Vector2 self) => (float)MathHelper.RadiansToDegrees(Math.Atan2(target.X - self.X, target.Y - self.Y));
 
-        public static float GetNotePathFromDegrees(this float degrees)
+        public static int GetNotePathFromDegrees(this float degrees)
         {
             if (degrees < 0) degrees += 360;
             if (degrees >= 360) degrees %= 360;
@@ -38,65 +25,7 @@ namespace osu.Game.Rulesets.Sentakki
                 if (SentakkiPlayfield.PATHANGLES[i] - degrees >= -22.5f && SentakkiPlayfield.PATHANGLES[i] - degrees <= 22.5f)
                     result = i;
             }
-            return SentakkiPlayfield.PATHANGLES[result];
+            return result;
         }
-
-        public static List<SentakkiHitObject> CreateTapFromTicks(HitObject obj, IBeatmap beatmap, IHasCurve curve, Random rng)
-        {
-            double spanDuration = curve.Duration / (curve.RepeatCount + 1);
-            bool isRepeatSpam = spanDuration < 75 && curve.RepeatCount > 0;
-
-            Vector2 CENTRE_POINT = new Vector2(256, 192);
-            List<SentakkiHitObject> hitObjects = new List<SentakkiHitObject>();
-            if (isRepeatSpam)
-                return hitObjects;
-
-            var difficulty = beatmap.BeatmapInfo.BaseDifficulty;
-
-            var objPosition = (obj as IHasPosition)?.Position ?? Vector2.Zero;
-
-            var controlPointInfo = beatmap.ControlPointInfo;
-            TimingControlPoint timingPoint = controlPointInfo.TimingPointAt(obj.StartTime);
-            DifficultyControlPoint difficultyPoint = controlPointInfo.DifficultyPointAt(obj.StartTime);
-
-            double scoringDistance = 100 * difficulty.SliderMultiplier * difficultyPoint.SpeedMultiplier;
-
-            var velocity = scoringDistance / timingPoint.BeatLength;
-            var tickDistance = scoringDistance / difficulty.SliderTickRate;
-
-            double legacyLastTickOffset = (obj as IHasLegacyLastTickOffset)?.LegacyLastTickOffset ?? 0;
-
-            foreach (var e in SliderEventGenerator.Generate(obj.StartTime, spanDuration, velocity, tickDistance, curve.Path.Distance, curve.RepeatCount + 1, legacyLastTickOffset))
-            {
-
-                var sliderEventPosition = (curve.CurvePositionAt(e.PathProgress / (curve.RepeatCount + 1)) + objPosition) * new Vector2(1, 0.5f);
-                sliderEventPosition.Y = 384 - sliderEventPosition.Y;
-
-                float angle;
-                switch (e.Type)
-                {
-                    case SliderEventType.Tick:
-                    case SliderEventType.Repeat:
-                        hitObjects.Add(new Tap
-                        {
-                            NoteColor = Color4.Orange,
-                            Angle = angle = (sliderEventPosition.GetDegreesFromPosition(CENTRE_POINT) + (22.5f * rng.Next(1, 6))).GetNotePathFromDegrees(),
-                            Samples = getTickSamples(obj.Samples),
-                            StartTime = e.Time,
-                            EndPosition = SentakkiExtensions.GetPosition(SentakkiPlayfield.INTERSECTDISTANCE, angle),
-                            Position = SentakkiExtensions.GetPosition(SentakkiPlayfield.NOTESTARTDISTANCE, angle),
-                        });
-                        break;
-                }
-            }
-            return hitObjects;
-        }
-
-        private static List<HitSampleInfo> getTickSamples(IList<HitSampleInfo> objSamples) => objSamples.Select(s => new HitSampleInfo
-        {
-            Bank = s.Bank,
-            Name = @"slidertick",
-            Volume = s.Volume
-        }).ToList();
     }
 }
