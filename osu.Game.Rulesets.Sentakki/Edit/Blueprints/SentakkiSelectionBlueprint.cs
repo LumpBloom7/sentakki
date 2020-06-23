e214b1d38b75fd6048f539478da8683166633984
Derrick Timmermans <derrick.timmermans@outlook.com>
2020-06-23 16:23:28 +0800

    Account for animation duration in ShouldBeAlive check
    Avoids an issue where notes can be selected before they first become visible

--- osu.Game.Rulesets.Sentakki/Edit/Blueprints/SentakkiSelectionBlueprint.cs
+++ osu.Game.Rulesets.Sentakki/Edit/Blueprints/SentakkiSelectionBlueprint.cs
@@ -4,6 +4,8 @@
 using osu.Game.Rulesets.Edit;
 using osu.Game.Rulesets.Objects.Drawables;
 using osu.Game.Rulesets.Sentakki.Objects;
+using osu.Game.Rulesets.Sentakki.Objects.Drawables;
+using osu.Game.Graphics.UserInterface;
 
 namespace osu.Game.Rulesets.Sentakki.Edit.Blueprints
 {
@@ -13,6 +15,9 @@ namespace osu.Game.Rulesets.Sentakki.Edit.Blueprints
         protected new T HitObject => (T)DrawableObject.HitObject;
 
         protected override bool AlwaysShowWhenSelected => true;
+
+        protected override bool ShouldBeAlive =>
+            (DrawableObject.IsAlive && DrawableObject.IsPresent && (DrawableObject as DrawableSentakkiHitObject).IsVisible) || (AlwaysShowWhenSelected && State == SelectionState.Selected);
 
         protected SentakkiSelectionBlueprint(DrawableHitObject drawableObject)
             : base(drawableObject)
