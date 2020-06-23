defa58d164b2a9815c8ec7c1b3e6ee11675611c3
Derrick Timmermans <derrick.timmermans@outlook.com>
2020-06-23 16:57:04 +0800

    Improve Tap note selection highlight

--- osu.Game.Rulesets.Sentakki/Edit/Blueprints/Taps/Components/TapPiece.cs
+++ osu.Game.Rulesets.Sentakki/Edit/Blueprints/Taps/Components/TapPiece.cs
@@ -1,10 +1,12 @@
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Containers;
 using osu.Game.Graphics;
 using osu.Game.Rulesets.Sentakki.Objects;
 using osu.Game.Rulesets.Sentakki.Objects.Drawables;
 using osuTK;
+using osuTK.Graphics;
 
 namespace osu.Game.Rulesets.Sentakki.Edit.Blueprints.Taps.Components
 {
@@ -13,19 +15,36 @@ namespace osu.Game.Rulesets.Sentakki.Edit.Blueprints.Taps.Components
         public TapPiece()
         {
             Size = new Vector2(80);
+            Colour = Colour4.GreenYellow;
 
             CornerRadius = Size.X / 2;
             CornerExponent = 2;
             Masking = true;
-            BorderColour = Colour4.GreenYellow;
-            BorderThickness = 3;
+            BorderColour = Colour4.White;
+            BorderThickness = 5;
 
-            InternalChild = new Box
-            {
-                Alpha = 0,
-                RelativeSizeAxes = Axes.Both,
-                AlwaysPresent = true
-            };
+            AddRangeInternal(new Drawable[]{
+                new Box{
+                    AlwaysPresent = true,
+                    Alpha = 0,
+                    RelativeSizeAxes = Axes.Both
+                },
+                new CircularContainer
+                {
+                    Size = new Vector2(20),
+                    Masking = true,
+                    BorderColour = Color4.Gray,
+                    BorderThickness = 2,
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Child = new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        AlwaysPresent = true,
+                        Colour = Color4.White,
+                    }
+                }
+            });
         }
 
         [BackgroundDependencyLoader]
