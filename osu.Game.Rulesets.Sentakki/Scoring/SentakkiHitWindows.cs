1412ae3b4536713040f68cda10bc04da02e5c8b0
Derrick Timmermans <derrick.timmermans@outlook.com>
2020-07-06 01:19:27 +0800

    Adjust hit windows to be closer to maimai

--- osu.Game.Rulesets.Sentakki/Scoring/SentakkiHitWindows.cs
+++ osu.Game.Rulesets.Sentakki/Scoring/SentakkiHitWindows.cs
@@ -18,5 +18,12 @@ namespace osu.Game.Rulesets.Sentakki.Scoring
                     return false;
             }
         }
+        protected override DifficultyRange[] GetRanges() => new DifficultyRange[]{
+            new DifficultyRange(HitResult.Miss, 144,144,144),
+            new DifficultyRange(HitResult.Meh, 144, 144, 144 ),
+            new DifficultyRange(HitResult.Good, 96, 96 , 96),
+            new DifficultyRange(HitResult.Great, 48, 48 , 48),
+            new DifficultyRange(HitResult.Perfect, 16, 16 ,16)
+        };
     }
 }
