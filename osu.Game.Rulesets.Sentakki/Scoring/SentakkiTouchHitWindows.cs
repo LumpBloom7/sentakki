06627a97023e9f4bb1038052fbeeca8b834854e3
Derrick Timmermans <derrick.timmermans@outlook.com>
2020-07-06 17:02:24 +0800

    Tidy up a bit

--- osu.Game.Rulesets.Sentakki/Scoring/SentakkiTouchHitWindows.cs
+++ osu.Game.Rulesets.Sentakki/Scoring/SentakkiTouchHitWindows.cs
@@ -6,7 +6,7 @@ namespace osu.Game.Rulesets.Sentakki.Scoring
     {
         protected override DifficultyRange[] GetRanges() => new DifficultyRange[]{
             new DifficultyRange(HitResult.Miss, 288, 288, 288),
-            new DifficultyRange(HitResult.Meh, 288, 288, 288 ),
+            new DifficultyRange(HitResult.Meh, 288, 288, 288),
             new DifficultyRange(HitResult.Good, 240, 240, 240),
             new DifficultyRange(HitResult.Great, 192, 192, 192),
             new DifficultyRange(HitResult.Perfect, 144, 144, 144)
