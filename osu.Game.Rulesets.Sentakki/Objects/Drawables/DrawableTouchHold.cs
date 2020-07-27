a969fb5fbe3acd48d9ebe7443c661bd69b80bac8
Derrick Timmermans <derrick.timmermans@outlook.com>
2020-07-27 23:42:00 +0800

    Fix TouchHolds being visible before they should after rewind

--- osu.Game.Rulesets.Sentakki/Objects/Drawables/DrawableTouchHold.cs
+++ osu.Game.Rulesets.Sentakki/Objects/Drawables/DrawableTouchHold.cs
@@ -6,6 +6,7 @@ using osu.Game.Rulesets.Sentakki.Objects.Drawables.Pieces;
 using osu.Game.Rulesets.Objects.Drawables;
 using osu.Game.Rulesets.Objects.Types;
 using osu.Game.Rulesets.Scoring;
+using osu.Game.Rulesets.Judgements;
 using osu.Game.Graphics;
 using osuTK;
 using osuTK.Graphics;
@@ -39,13 +40,25 @@ namespace osu.Game.Rulesets.Sentakki.Objects.Drawables
             AddRangeInternal(new Drawable[] {
                 circle = new TouchHoldCircle(){ Duration = hitObject.Duration },
             });
+
+            OnNewResult += (DrawableHitObject obj, JudgementResult result) =>
+            {
+                AccentColour.Value = colours.ForHitResult(result.Type);
+            };
+            OnRevertResult += (DrawableHitObject obj, JudgementResult result) =>
+            {
+                AccentColour.Value = Color4.HotPink;
+            };
+            activated.BindValueChanged(b =>
+            {
+                circle.FadeTo(b.NewValue ? 1 : .5f, 100);
+                circle.ScaleTo(b.NewValue ? 1 : .8f, 100);
+            });
         }
 
         private double timeHeld = 0;
-        private bool activated => Auto || ((SentakkiActionInputManager?.PressedActions.Any() ?? false) && IsHovered);
 
-        // This is used to reset the animation I used to achieve the judgement feedback.
-        private bool needReset = false;
+        private readonly BindableBool activated = new BindableBool(false);
 
         protected override void CheckForResult(bool userTriggered, double timeOffset)
         {
@@ -54,7 +67,6 @@ namespace osu.Game.Rulesets.Sentakki.Objects.Drawables
             if (userTriggered || Time.Current < (HitObject as IHasDuration)?.EndTime)
                 return;
 
-            FinishTransforms(true);
             double result = timeHeld / (HitObject as IHasDuration).Duration;
 
             ApplyResult(r =>
@@ -70,7 +82,6 @@ namespace osu.Game.Rulesets.Sentakki.Objects.Drawables
                 else if (Time.Current >= (HitObject as IHasDuration)?.EndTime)
                     r.Type = HitResult.Miss;
             });
-            needReset = true;
         }
 
         [BackgroundDependencyLoader(true)]
@@ -81,11 +92,6 @@ namespace osu.Game.Rulesets.Sentakki.Objects.Drawables
 
         [Resolved]
         private OsuColour colours { get; set; }
-        private Color4 currentColour
-        {
-            get => AccentColour.Value;
-            set => AccentColour.Value = value;
-        }
 
         /// <summary>
         /// Time at which the user started holding this hold note. Null if the user is not holding this hold note.
@@ -94,7 +100,6 @@ namespace osu.Game.Rulesets.Sentakki.Objects.Drawables
 
         protected override void UpdateInitialTransforms()
         {
-            circle.Progress.Current.Value = 0;
             double fadeIn = AnimationDuration.Value * GameplaySpeed;
             using (BeginAbsoluteSequence(HitObject.StartTime - fadeIn, true))
             {
@@ -109,58 +114,38 @@ namespace osu.Game.Rulesets.Sentakki.Objects.Drawables
         protected override void Update()
         {
             base.Update();
+            activated.Value = Time.Current >= HitObject.StartTime
+                            && Time.Current <= (HitObject as IHasDuration)?.EndTime
+                            && (Auto || ((SentakkiActionInputManager?.PressedActions.Any() ?? false) && IsHovered));
+
             if (Result.HasResult) return;
-            // Used to prevent rewind visual issues
-            if (needReset)
-            {
-                circle.Size = Vector2.One;
-                currentColour = Color4.HotPink;
-                needReset = false;
-            }
 
             // Input and feedback
             if (Time.Current >= HitObject.StartTime && Time.Current <= (HitObject as IHasDuration)?.EndTime)
             {
-                if (activated)
+                if (activated.Value)
                 {
                     double prevProg = timeHeld / (HitObject as IHasDuration).Duration;
-                    timeHeld += Clock.ElapsedFrameTime;
+                    timeHeld += Time.Elapsed;
                     double progress = timeHeld / (HitObject as IHasDuration).Duration;
 
                     if (progress >= .25f && prevProg < .25f)
                     {
                         circle.ResizeTo(1.033f, 100);
-                        this.TransformTo(nameof(currentColour), colours.ForHitResult(HitResult.Meh), 100);
+                        this.TransformBindableTo(AccentColour, colours.ForHitResult(HitResult.Meh), 100);
                     }
                     else if (progress >= .50f && prevProg < .50f)
                     {
                         circle.ResizeTo(1.066f, 100);
-                        this.TransformTo(nameof(currentColour), colours.ForHitResult(HitResult.Good), 100);
+                        this.TransformBindableTo(AccentColour, colours.ForHitResult(HitResult.Good), 100);
                     }
                     else if (progress >= .75f && prevProg < .75f)
                     {
                         circle.ResizeTo(1.1f, 100);
-                        this.TransformTo(nameof(currentColour), colours.ForHitResult(HitResult.Great), 100);
-                    }
-
-                    if (HoldStartTime == null)
-                    {
-                        circle.FadeTo(1, 100);
-                        circle.ScaleTo(1, 100);
-                        HoldStartTime = Clock.CurrentTime;
-                    }
-                }
-                else
-                {
-                    if (HoldStartTime != null)
-                    {
-                        circle.FadeTo(.5f, 100);
-                        circle.ScaleTo(.8f, 200);
-                        HoldStartTime = null;
+                        this.TransformBindableTo(AccentColour, colours.ForHitResult(HitResult.Great), 100);
                     }
                 }
             }
-            base.Update();
         }
 
         protected override void UpdateStateTransforms(ArmedState state)
