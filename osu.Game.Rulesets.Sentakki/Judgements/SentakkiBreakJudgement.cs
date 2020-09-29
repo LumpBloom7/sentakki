1b56ce5d7132029044752ec5c994012e76f03392
Derrick Timmermans <derrick.timmermans@outlook.com>
2020-09-29 19:00:53 +0200

    Remove the use of custom HitObject scores
    They simply do nothing now, and Breaks are in all senses identical to normal notes.

--- osu.Game.Rulesets.Sentakki/Judgements/SentakkiBreakJudgement.cs
+++ osu.Game.Rulesets.Sentakki/Judgements/SentakkiBreakJudgement.cs
@@ -5,44 +5,5 @@ namespace osu.Game.Rulesets.Sentakki.Judgements
     public class SentakkiBreakJudgement : SentakkiJudgement
     {
         public override HitResult MaxResult => HitResult.Perfect;
-        protected override int NumericResultFor(HitResult result)
-        {
-            switch (result)
-            {
-                default:
-                    return 0;
-
-                case HitResult.Meh:
-                    return 1000;
-
-                case HitResult.Good:
-                    return 2000;
-
-                case HitResult.Great:
-                    return 2500;
-                case HitResult.Perfect:
-                    return 2600;
-            }
-        }
-
-        protected override double HealthIncreaseFor(HitResult result)
-        {
-            switch (result)
-            {
-                default:
-                    return 0;
-
-                case HitResult.Miss:
-                    return -0.4;
-
-                case HitResult.Meh:
-                case HitResult.Good:
-                case HitResult.Great:
-                    return 0.4;
-
-                case HitResult.Perfect:
-                    return 0.6;
-            }
-        }
     }
 }
--- osu.Game.Rulesets.Sentakki/Judgements/SentakkiJudgement.cs
+++ osu.Game.Rulesets.Sentakki/Judgements/SentakkiJudgement.cs
@@ -5,45 +5,6 @@ namespace osu.Game.Rulesets.Sentakki.Judgements
 {
     public class SentakkiJudgement : Judgement
     {
-        public override HitResult MaxResult => HitResult.Great;
-
-        protected override int NumericResultFor(HitResult result)
-        {
-            switch (result)
-            {
-                default:
-                    return 0;
-
-                case HitResult.Meh:
-                    return 250;
-
-                case HitResult.Good:
-                    return 400;
-
-                case HitResult.Great:
-                case HitResult.Perfect:
-                    return 500;
-            }
-        }
-
-        protected override double HealthIncreaseFor(HitResult result)
-        {
-            switch (result)
-            {
-                default:
-                    return 0;
-
-                case HitResult.Miss:
-                    return -0.1;
-
-                case HitResult.Meh:
-                case HitResult.Good:
-                case HitResult.Great:
-                    return 0.2;
-
-                case HitResult.Perfect:
-                    return 0.3;
-            }
-        }
+        public override HitResult MaxResult => HitResult.Perfect;
     }
 }
