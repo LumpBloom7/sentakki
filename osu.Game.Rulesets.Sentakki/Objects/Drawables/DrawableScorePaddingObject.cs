bf2f27b8a9172e5b12b92994f7437eb741614332
Derrick Timmermans <derrick.timmermans@outlook.com>
2020-11-07 13:15:19 +0100

    Move DrawableScorePaddingObject out of the global namespace
    Unacceptable Bloom... UNACCEPTABLE!

--- osu.Game.Rulesets.Sentakki/Objects/Drawables/DrawableScorePaddingObject.cs
+++ osu.Game.Rulesets.Sentakki/Objects/Drawables/DrawableScorePaddingObject.cs
@@ -1,18 +1,20 @@
 using System;
 using osu.Game.Rulesets.Judgements;
 using osu.Game.Rulesets.Objects.Drawables;
-using osu.Game.Rulesets.Sentakki.Objects;
 
-public class DrawableScorePaddingObject : DrawableHitObject<ScorePaddingObject>
+namespace osu.Game.Rulesets.Sentakki.Objects.Drawables
 {
-    public DrawableScorePaddingObject(ScorePaddingObject hitObject)
-        : base(hitObject)
+    public class DrawableScorePaddingObject : DrawableHitObject<ScorePaddingObject>
     {
-    }
+        public DrawableScorePaddingObject(ScorePaddingObject hitObject)
+            : base(hitObject)
+        {
+        }
 
-    public new void ApplyResult(Action<JudgementResult> application)
-    {
-        if (!Result.HasResult)
-            base.ApplyResult(application);
+        public new void ApplyResult(Action<JudgementResult> application)
+        {
+            if (!Result.HasResult)
+                base.ApplyResult(application);
+        }
     }
 }
