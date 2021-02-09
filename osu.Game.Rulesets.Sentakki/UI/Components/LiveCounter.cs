6788d4eccb6feb1f31fa1f580530b8bae055632a
Derrick Timmermans <derrick.timmermans@outlook.com>
2021-02-09 20:51:49 +0100

    Add heartbeat animation

--- osu.Game.Rulesets.Sentakki/UI/Components/LiveCounter.cs
+++ osu.Game.Rulesets.Sentakki/UI/Components/LiveCounter.cs
@@ -1,5 +1,8 @@
+using System;
+using osu.Framework.Audio.Track;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
+using osu.Game.Beatmaps.ControlPoints;
 using osu.Game.Graphics;
 using osu.Game.Graphics.Containers;
 using osu.Game.Graphics.Sprites;
@@ -7,7 +10,7 @@ using osuTK.Graphics;
 
 namespace osu.Game.Rulesets.Sentakki.UI.Components
 {
-    public class LiveCounter : ShakeContainer
+    public class LiveCounter : BeatSyncedContainer
     {
         public BindableInt LivesLeft = new BindableInt(300);
 
@@ -17,7 +20,6 @@ namespace osu.Game.Rulesets.Sentakki.UI.Components
         {
             Anchor = Anchor.Centre;
             Origin = Anchor.Centre;
-            ShakeMagnitude = 4;
 
             InternalChildren = new Drawable[]{
 
@@ -36,6 +38,35 @@ namespace osu.Game.Rulesets.Sentakki.UI.Components
                 this.FadeColour(Color4.Red, 160).Then().FadeColour(Color4.White, 320);
                 Shake();
             }, true);
+        }
+
+        public void Shake(double? maximumLength = null)
+        {
+            const float shake_amount = 8;
+            const double shake_duration = 80;
+
+            // if we don't have enough time, don't bother shaking.
+            if (maximumLength < shake_duration * 2)
+                return;
+
+            var sequence = this.MoveToX(shake_amount, shake_duration / 2, Easing.OutSine).Then()
+                               .MoveToX(-shake_amount, shake_duration, Easing.InOutSine).Then();
+
+            // if we don't have enough time for the second shake, skip it.
+            if (!maximumLength.HasValue || maximumLength >= shake_duration * 4)
+            {
+                sequence = sequence
+                           .MoveToX(shake_amount, shake_duration, Easing.InOutSine).Then()
+                           .MoveToX(-shake_amount, shake_duration, Easing.InOutSine).Then();
+            }
+
+            sequence.MoveToX(0, shake_duration / 2, Easing.InSine);
+        }
+
+        protected override void OnNewBeat(int beatIndex, TimingControlPoint timingPoint, EffectControlPoint effectPoint, ChannelAmplitudes amplitudes)
+        {
+            if (beatIndex % (int)timingPoint.TimeSignature == 0)
+                this.ScaleTo(1.1f, 200).Then().ScaleTo(1, 120).Then().ScaleTo(1.1f, 160).Then().ScaleTo(1, 320);
         }
     }
 }
