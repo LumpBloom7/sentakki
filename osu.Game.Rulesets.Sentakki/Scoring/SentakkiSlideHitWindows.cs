1da2a40fd261ffbc1a27d2666b2cf5ce8c276bed
Derrick Timmermans <derrick.timmermans@outlook.com>
2021-02-13 19:43:51 +0100

    Reimplement HR mod Halves all hitwindow ranges

--- osu.Game.Rulesets.Sentakki/Scoring/SentakkiSlideHitWindows.cs
+++ osu.Game.Rulesets.Sentakki/Scoring/SentakkiSlideHitWindows.cs
@@ -5,10 +5,10 @@ namespace osu.Game.Rulesets.Sentakki.Scoring
     public class SentakkiSlideHitWindows : SentakkiHitWindows
     {
         protected override DifficultyRange[] GetRanges() => new DifficultyRange[]{
-            new DifficultyRange(HitResult.Miss, 576, 576, 576),
-            new DifficultyRange(HitResult.Good, 576, 576, 576),
-            new DifficultyRange(HitResult.Great, 416, 416, 416),
-            new DifficultyRange(HitResult.Perfect, 288, 288, 288)
+            new DifficultyRange(HitResult.Miss, 576, 576, 288),
+            new DifficultyRange(HitResult.Good, 576, 576, 288),
+            new DifficultyRange(HitResult.Great, 416, 416, 208),
+            new DifficultyRange(HitResult.Perfect, 288, 288, 144)
         };
     }
 }
