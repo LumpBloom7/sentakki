e45e35b1ba247639384a3238d8e70ba28ca6b663
Derrick Timmermans <derrick.timmermans@outlook.com>
2021-03-04 17:40:55 +0100

    Extend SentakkiSlidePath to store segments

--- osu.Game.Rulesets.Sentakki/Objects/SentakkiSlidePath.cs
+++ osu.Game.Rulesets.Sentakki/Objects/SentakkiSlidePath.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using osu.Game.Rulesets.Objects;
 
 namespace osu.Game.Rulesets.Sentakki.Objects
@@ -10,16 +11,27 @@ namespace osu.Game.Rulesets.Sentakki.Objects
         public readonly SliderPath Path;
 
         // The minimum duration that this pattern can have, used in converts
-        public readonly double MinDuration;
+        public double MinDuration => TotalDistance / 2;
 
         // The maximum duration that this pattern can have, used in converts.
         // While it is completely playable even beyond this value, it would look awkward for shorter slides
         public double MaxDuration => MinDuration * 10;
 
-        public SentakkiSlidePath(PathControlPoint[] pathControlPoints, int endLane)
+        public double TotalDistance;
+
+        public readonly SliderPath[] SlideSegments;
+
+        public SentakkiSlidePath(SliderPath segment, int endLane)
         {
-            Path = new SliderPath(pathControlPoints);
-            MinDuration = Path.Distance / 2;
+            SlideSegments = new SliderPath[] { segment };
+            TotalDistance = SlideSegments.Sum(p => p.Distance);
+            EndLane = endLane;
+        }
+
+        public SentakkiSlidePath(SliderPath[] segments, int endLane)
+        {
+            SlideSegments = segments;
+            TotalDistance = SlideSegments.Sum(p => p.Distance);
             EndLane = endLane;
         }
     }
