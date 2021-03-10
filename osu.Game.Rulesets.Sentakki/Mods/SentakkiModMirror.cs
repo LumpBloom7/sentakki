0e42dab07f3820474ddf115356a5d24faaea23f6
Derrick Timmermans <derrick.timmermans@outlook.com>
2021-03-10 15:34:24 +0100

    Change wording a bit

--- osu.Game.Rulesets.Sentakki/Mods/SentakkiModMirror.cs
+++ osu.Game.Rulesets.Sentakki/Mods/SentakkiModMirror.cs
@@ -19,15 +19,15 @@ namespace osu.Game.Rulesets.Sentakki.Mods
 
         public override bool RequiresConfiguration => true;
 
-        [SettingSource("Mirror along X Axis")]
-        public BindableBool XAxis { get; } = new BindableBool
+        [SettingSource("⇅ Mirror vertically", "Mirror entire playfield across the x-axis")]
+        public BindableBool VerticalMirrored { get; } = new BindableBool
         {
             Default = false,
             Value = false
         };
 
-        [SettingSource("Mirror along Y Axis")]
-        public BindableBool YAxis { get; } = new BindableBool
+        [SettingSource("⇔ Mirror horizontally", "Mirror entire playfield across the y-axis")]
+        public BindableBool HorizontalMirrored { get; } = new BindableBool
         {
             Default = false,
             Value = false
@@ -35,16 +35,18 @@ namespace osu.Game.Rulesets.Sentakki.Mods
 
         public void ApplyToBeatmap(IBeatmap beatmap)
         {
-            //Mirroring time
-            bool mirrored = XAxis.Value ^ YAxis.Value;
+            // Mirroring in both directions at the same time is equivalent to an 180deg rotation
+            // Because of that, we wouldn't need to swap slide paths with their mirrored counterpart
+            bool mirrored = VerticalMirrored.Value ^ HorizontalMirrored.Value;
+
             beatmap.HitObjects.OfType<SentakkiLanedHitObject>().ForEach(laned =>
             {
-                if (YAxis.Value)
+                if (HorizontalMirrored.Value)
                 {
                     laned.Lane = 7 - laned.Lane;
                 }
 
-                if (XAxis.Value)
+                if (VerticalMirrored.Value)
                 {
                     laned.Lane = (3 - laned.Lane) % 8;
                     if (laned.Lane < 0) laned.Lane += 8;
@@ -57,10 +59,10 @@ namespace osu.Game.Rulesets.Sentakki.Mods
             beatmap.HitObjects.OfType<Touch>().ForEach(touch =>
             {
                 Vector2 newPosition = touch.Position;
-                if (YAxis.Value)
+                if (HorizontalMirrored.Value)
                     newPosition.X = -touch.Position.X;
 
-                if (XAxis.Value)
+                if (VerticalMirrored.Value)
                     newPosition.Y = -touch.Position.Y;
 
                 touch.Position = newPosition;
