137d25069d4ca1053749afc49a65472fdde477d6
Derrick Timmermans <derrick.timmermans@outlook.com>
2021-04-24 13:35:31 +0200

    Don't attempt sorting proxy container every update

--- osu.Game.Rulesets.Sentakki/UI/SortedDrawableProxyContainer.cs
+++ osu.Game.Rulesets.Sentakki/UI/SortedDrawableProxyContainer.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Rulesets.Objects.Drawables;
@@ -11,29 +12,27 @@ namespace osu.Game.Rulesets.Sentakki.UI
         {
             RelativeSizeAxes = Axes.Both;
         }
-        private readonly Dictionary<Drawable, DrawableHitObject> drawableMap = new Dictionary<Drawable, DrawableHitObject>();
 
-        public void Add(Drawable slideBodyProxy, DrawableHitObject hitObject)
+        private readonly Dictionary<Drawable, IBindable<double>> startTimeMap = new Dictionary<Drawable, IBindable<double>>();
+
+        public void Add(Drawable proxy, DrawableHitObject hitObject)
         {
-            drawableMap.Add(slideBodyProxy, hitObject);
-            AddInternal(slideBodyProxy);
+
+            var startTimeBindable = hitObject.StartTimeBindable.GetBoundCopy();
+            startTimeBindable.BindValueChanged(s =>
+            {
+                if (LoadState >= LoadState.Ready)
+                    SortInternal();
+            });
+            startTimeMap.Add(proxy, startTimeBindable);
+            AddInternal(proxy);
         }
 
         protected override int Compare(Drawable x, Drawable y)
         {
             // Put earlier hitobjects towards the end of the list, so they show above the rest
-            int i = getStartTimeOf(drawableMap[y]).CompareTo(getStartTimeOf(drawableMap[x]));
+            int i = startTimeMap[y].Value.CompareTo(startTimeMap[x].Value);
             return i == 0 ? base.Compare(x, y) : i;
-        }
-
-        private double getStartTimeOf(DrawableHitObject hitObject) => hitObject.StartTimeBindable.Value;
-
-        protected override void Update()
-        {
-            base.Update();
-
-            // Used to resolve a potential edge case bug that could happen when abusing the animation speed slider or gameplay rewind
-            SortInternal();
         }
     }
 }
