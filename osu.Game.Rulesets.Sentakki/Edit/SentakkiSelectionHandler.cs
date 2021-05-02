be49d5745fe480de59dedb76554ad9a1ad32d3ac
Derrick Timmermans <derrick.timmermans@outlook.com>
2021-05-02 20:21:30 +0200

    Implement break toggle in context menu for laned HOs

--- osu.Game.Rulesets.Sentakki/Edit/SentakkiSelectionHandler.cs
+++ osu.Game.Rulesets.Sentakki/Edit/SentakkiSelectionHandler.cs
@@ -1,4 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics.UserInterface;
 using osu.Game.Extensions;
+using osu.Game.Graphics.UserInterface;
+using osu.Game.Rulesets.Edit;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Sentakki.Objects;
 using osu.Game.Screens.Edit.Compose.Components;
@@ -8,6 +16,24 @@ namespace osu.Game.Rulesets.Sentakki.Edit
 {
     public class SentakkiSelectionHandler : EditorSelectionHandler
     {
+        private readonly Bindable<TernaryState> selectionBreakState = new Bindable<TernaryState>();
+
+        public SentakkiSelectionHandler()
+        {
+            selectionBreakState.ValueChanged += s =>
+            {
+                switch (s.NewValue)
+                {
+                    case TernaryState.False:
+                        setBreakState(false);
+                        break;
+
+                    case TernaryState.True:
+                        setBreakState(true);
+                        break;
+                }
+            };
+        }
         public override bool HandleMovement(MoveSelectionEvent<HitObject> moveEvent)
         {
             if (SelectedBlueprints.Count > 1)
@@ -37,6 +63,37 @@ namespace osu.Game.Rulesets.Sentakki.Edit
                     break;
             }
             return true;
+        }
+
+        private void setBreakState(bool state)
+        {
+            var lhos = EditorBeatmap.SelectedHitObjects.OfType<SentakkiLanedHitObject>();
+
+            EditorBeatmap.BeginChange();
+
+            foreach (var lho in lhos)
+            {
+                if (lho.Break == state)
+                    continue;
+
+                lho.Break = state;
+                EditorBeatmap.Update(lho);
+            }
+
+            EditorBeatmap.EndChange();
+        }
+
+        protected override void UpdateTernaryStates()
+        {
+            base.UpdateTernaryStates();
+
+            selectionBreakState.Value = GetStateFromSelection(EditorBeatmap.SelectedHitObjects.OfType<SentakkiLanedHitObject>(), h => h.Break);
+        }
+
+        protected override IEnumerable<MenuItem> GetContextMenuItemsForSelection(IEnumerable<SelectionBlueprint<HitObject>> selection)
+        {
+            if (selection.Any(s => s.Item is SentakkiLanedHitObject))
+                yield return new TernaryStateMenuItem("Break") { State = { BindTarget = selectionBreakState } };
         }
     }
 }
