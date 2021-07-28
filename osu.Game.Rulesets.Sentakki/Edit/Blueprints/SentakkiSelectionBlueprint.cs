956d740a4820ce83f96b48fb92c55dadc2e239f0
Derrick Timmermans <derrick.timmermans@outlook.com>
2021-07-28 21:30:46 +0200

    Remove redundant override for blueprint lifetimes

--- osu.Game.Rulesets.Sentakki/Edit/Blueprints/SentakkiSelectionBlueprint.cs
+++ osu.Game.Rulesets.Sentakki/Edit/Blueprints/SentakkiSelectionBlueprint.cs
@@ -9,19 +9,5 @@ namespace osu.Game.Rulesets.Sentakki.Edit.Blueprints
         protected override bool AlwaysShowWhenSelected => true;
 
         public SentakkiSelectionBlueprint(T hitObject) : base(hitObject) { }
-
-        protected override bool ShouldBeAlive
-        {
-            get
-            {
-                if (DrawableObject.Time.Current < DrawableObject.LifetimeStart || DrawableObject.Time.Current >= DrawableObject.LifetimeEnd)
-                {
-                    if (AlwaysShowWhenSelected)
-                        return State == SelectionState.Selected;
-                    return false;
-                }
-                return true;
-            }
-        }
     }
 }
