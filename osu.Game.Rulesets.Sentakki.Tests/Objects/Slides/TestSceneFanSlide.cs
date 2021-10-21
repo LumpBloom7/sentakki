9027a42db30567359d31b60980c5caf6e40ffe46
Derrick Timmermans <derrick.timmermans@outlook.com>
2021-10-21 18:39:10 +0200

    Create test scene for FanSlide

---
+++ osu.Game.Rulesets.Sentakki.Tests/Objects/Slides/TestSceneFanSlide.cs
@@ -0,0 +1,55 @@
+using NUnit.Framework;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Rulesets.Sentakki.Objects;
+using osu.Game.Rulesets.Sentakki.Objects.Drawables.Pieces;
+using osu.Game.Rulesets.Sentakki.UI;
+using osu.Game.Rulesets.Sentakki.UI.Components;
+using osu.Game.Tests.Visual;
+using osuTK;
+
+namespace osu.Game.Rulesets.Sentakki.Tests.Objects.Slides
+{
+    [TestFixture]
+    public class TestSceneFanSlide : OsuTestScene
+    {
+        protected override Ruleset CreateRuleset() => new SentakkiRuleset();
+
+        protected int StartPath;
+        protected int EndPath;
+
+        private readonly SlideFanVisual slide;
+        private readonly Container nodes;
+
+        public TestSceneFanSlide()
+        {
+
+            Add(new SentakkiRing()
+            {
+                RelativeSizeAxes = Axes.None,
+                Size = new Vector2(SentakkiPlayfield.RINGSIZE)
+            });
+
+            Add(slide = new SlideFanVisual()
+            {
+            });
+
+            AddSliderStep("Progress", 0.0f, 1.0f, 0.0f, p =>
+            {
+                slide.Progress = p;
+            });
+
+            Add(nodes = new Container()
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+            });
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            slide.Path = SlidePaths.GenerateStraightPattern(4);
+        }
+    }
+}
---
+++ osu.Game.Rulesets.Sentakki/Objects/Drawables/Pieces/SlideFanVisual.cs
@@ -0,0 +1,185 @@
+using System;
+using System.Linq;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Pooling;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using osu.Game.Rulesets.Objects;
+using osu.Game.Rulesets.Sentakki.Configuration;
+
+namespace osu.Game.Rulesets.Sentakki.Objects.Drawables.Pieces
+{
+    public class SlideFanVisual : CompositeDrawable
+    {
+        // This will be proxied, so a must.
+        public override bool RemoveWhenNotAlive => false;
+
+        private double progress;
+        public double Progress
+        {
+            get => progress;
+            set
+            {
+                progress = value;
+                updateProgress();
+            }
+        }
+        private SentakkiSlidePath path;
+
+        public SentakkiSlidePath Path
+        {
+            get => path;
+            set
+            {
+                path = value;
+                updateVisuals();
+                updateProgress();
+            }
+        }
+
+        private Container<SlideFanChevron> chevrons;
+
+        private readonly BindableBool snakingIn = new BindableBool(true);
+
+        public SlideFanVisual()
+        {
+            Anchor = Anchor.Centre;
+            Origin = Anchor.Centre;
+        }
+
+
+        [BackgroundDependencyLoader(true)]
+        private void load(SentakkiRulesetConfigManager sentakkiConfig)
+        {
+            sentakkiConfig?.BindWith(SentakkiRulesetSettings.SnakingSlideBody, snakingIn);
+
+            AddRangeInternal(new Drawable[]{
+                chevrons = new Container<SlideFanChevron>(),
+            });
+        }
+
+        private const int chevrons_per_eith = 8;
+        private const double ring_radius = 297;
+        private const double chevrons_per_distance = (chevrons_per_eith * 8) / (2 * Math.PI * ring_radius);
+        private const double endpoint_distance = 30; // margin for each end
+
+        private static int chevronsInContinuousPath(SliderPath path)
+        {
+            return (int)Math.Ceiling((path.Distance - (2 * endpoint_distance)) * chevrons_per_distance);
+        }
+
+        private void updateVisuals()
+        {
+            chevrons.Clear(false);
+
+            double runningDistance = 0;
+            foreach (var path in path.SlideSegments)
+            {
+                var chevronCount = chevronsInContinuousPath(path);
+                var totalDistance = path.Distance;
+                var safeDistance = totalDistance - (endpoint_distance * 2);
+
+                var previousPosition = path.PositionAt(0);
+                for (int i = 0; i < chevronCount; i++)
+                {
+                    var progress = (double)i / (chevronCount - 1); // from 0 to 1, both inclusive
+                    var distance = (progress * safeDistance) + endpoint_distance;
+                    progress = distance / totalDistance;
+                    var position = path.PositionAt(progress);
+                    var angle = previousPosition.GetDegreesFromPosition(position);
+
+                    var chevron = new SlideFanChevron();
+                    chevron.Position = position;
+                    chevron.Progress = (runningDistance + distance) / this.path.TotalDistance;
+                    chevron.Rotation = angle;
+                    chevron.Depth = chevrons.Count;
+                    chevrons.Add(chevron);
+
+                    previousPosition = position;
+                }
+                runningDistance += totalDistance;
+            }
+        }
+
+        private void updateProgress()
+        {
+            for (int i = 0; i < chevrons.Count; i++)
+            {
+                chevrons[i].UpdateProgress(progress);
+            }
+        }
+
+        public void PerformEntryAnimation(double duration)
+        {
+            if (snakingIn.Value)
+            {
+                double fadeDuration = duration / chevrons.Count;
+                double currentOffset = duration / 2;
+                for (int j = chevrons.Count - 1; j >= 0; j--)
+                {
+                    var chevron = chevrons[j];
+                    chevron.FadeOut().Delay(currentOffset).FadeIn(fadeDuration * 2).Finally(finalSteps);
+                    currentOffset += fadeDuration / 2;
+                }
+            }
+            else
+            {
+                chevrons.FadeOut().Delay(duration / 2).FadeIn(duration / 2);
+            }
+
+            void finalSteps(SlideFanChevron chevron) => chevron.UpdateProgress(progress);
+        }
+
+        public void PerformExitAnimation(double duration)
+        {
+            int chevronsLeft = chevrons.Count(c => c.Alpha != 0);
+            double fadeDuration = duration / chevronsLeft;
+            double currentOffset = 0;
+            for (int j = chevrons.Count - 1; j >= 0; j--)
+            {
+                var chevron = chevrons[j];
+                if (chevron.Alpha == 0)
+                {
+                    continue;
+                }
+                chevron.Delay(currentOffset).FadeOut(fadeDuration * 2);
+                currentOffset += fadeDuration / 2;
+            }
+        }
+
+        public void Free()
+        {
+            chevrons.Clear(false);
+        }
+
+        public class SlideFanChevron : CompositeDrawable
+        {
+            public double Progress;
+
+            public SlideFanChevron()
+            {
+                Anchor = Anchor.Centre;
+                Origin = Anchor.Centre;
+            }
+
+            [BackgroundDependencyLoader]
+            private void load(TextureStore textures)
+            {
+                AddInternal(new Sprite
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Texture = textures.Get("FanSlide"),
+                });
+            }
+
+            public void UpdateProgress(double progress)
+            {
+                Alpha = progress >= Progress ? 0 : 1;
+            }
+        }
+    }
+}
