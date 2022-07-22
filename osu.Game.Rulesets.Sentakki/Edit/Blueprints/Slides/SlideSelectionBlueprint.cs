9638424d4233bbca540bafc6f33167f02ac8d850
Derrick Timmermans <derrick.timmermans@outlook.com>
2022-07-22 20:46:27 +0200

    Support multi-slides in SlideSelectionBlueprint

--- osu.Game.Rulesets.Sentakki/Edit/Blueprints/Slides/SlideSelectionBlueprint.cs
+++ osu.Game.Rulesets.Sentakki/Edit/Blueprints/Slides/SlideSelectionBlueprint.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Collections.Generic;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Primitives;
 using osu.Game.Rulesets.Sentakki.Objects;
@@ -14,16 +15,29 @@ namespace osu.Game.Rulesets.Sentakki.Edit.Blueprints.Slides
     {
         public new DrawableSlide DrawableObject => (DrawableSlide)base.DrawableObject;
 
-        private readonly SlideStarHighlight starHighlight;
-        private readonly SlideBodyHighlight bodyHighlight;
+        private readonly List<SlideStarHighlight> starHighlights = new List<SlideStarHighlight>();
+        private readonly List<SlideBodyHighlight> bodyHighlights = new List<SlideBodyHighlight>();
 
         public SlideSelectionBlueprint(Slide hitObject)
             : base(hitObject)
         {
-            InternalChildren = new Drawable[]{
-                bodyHighlight = new SlideBodyHighlight(),
-                starHighlight = new SlideStarHighlight()
-            };
+            foreach (var body in hitObject.SlideInfoList)
+            {
+                switch (body.ID)
+                {
+                    case 34: // FanID
+                        for (int i = 0; i < 3; ++i)
+                            starHighlights.Add(new SlideStarHighlight());
+                        break;
+                    default:
+                        starHighlights.Add(new SlideStarHighlight());
+                        bodyHighlights.Add(new SlideBodyHighlight());
+                        break;
+                }
+            }
+
+            AddRangeInternal(starHighlights);
+            AddRangeInternal(bodyHighlights);
         }
 
         protected override void Update()
@@ -36,34 +50,51 @@ namespace osu.Game.Rulesets.Sentakki.Edit.Blueprints.Slides
 
             if (DrawableObject.Time.Current < DrawableObject.HitObject.StartTime)
             {
-                starHighlight.Rotation = SlideTap.HitObject.Lane.GetRotationForLane();
-                starHighlight.Note.Position = new Vector2(0, Math.Max(SlideTap.TapVisual.Y, -SentakkiPlayfield.INTERSECTDISTANCE));
-                starHighlight.Note.Scale = SlideTap.TapVisual.Scale;
-                starHighlight.Note.Rotation = (SlideTap.TapVisual as SlideTapPiece).Stars.Rotation;
+                foreach (var starHighlight in starHighlights)
+                {
+                    starHighlight.Rotation = SlideTap.HitObject.Lane.GetRotationForLane();
+                    starHighlight.Note.Position = new Vector2(0, Math.Max(SlideTap.TapVisual.Y, -SentakkiPlayfield.INTERSECTDISTANCE));
+                    starHighlight.Note.Scale = SlideTap.TapVisual.Scale;
+                    starHighlight.Note.Rotation = (SlideTap.TapVisual as SlideTapPiece).Stars.Rotation;
+                }
             }
             else
             {
-                if (FirstSlideBody is null)
-                    return;
-
-                starHighlight.Rotation = SlideTap.HitObject.Lane.GetRotationForLane() - 22.5f;
-                starHighlight.Note.Position = FirstSlideBody.SlideStars[0].Position;
-                starHighlight.Note.Scale = FirstSlideBody.SlideStars[0].Scale;
-                starHighlight.Note.Rotation = FirstSlideBody.SlideStars[0].Rotation;
+                int starIndex = 0;
+                foreach (var slideBody in DrawableObject.SlideBodies)
+                {
+                    foreach (var slideStar in slideBody.SlideStars)
+                    {
+                        starHighlights[starIndex].Rotation = SlideTap.HitObject.Lane.GetRotationForLane() - 22.5f;
+                        starHighlights[starIndex].Note.Position = slideStar.Position;
+                        starHighlights[starIndex].Note.Scale = slideStar.Scale;
+                        starHighlights[starIndex].Note.Rotation = slideStar.Rotation;
+                        ++starIndex;
+                    }
+                }
             }
 
-            if (FirstSlideBody is null)
-                return;
-
-            bodyHighlight.Rotation = SlideTap.HitObject.Lane.GetRotationForLane() - 22.5f;
-            bodyHighlight.Path.Vertices = DrawableObject.SlideBodies[0].HitObject.SlideInfo.SlidePath.Vertices;
+            for (int i = 0; i < DrawableObject.SlideBodies.Count; ++i)
+            {
+                bodyHighlights[i].Rotation = SlideTap.HitObject.Lane.GetRotationForLane() - 22.5f;
+                bodyHighlights[i].Path.Vertices = DrawableObject.SlideBodies[i].HitObject.SlideInfo.SlidePath.Vertices;
+            }
         }
 
-        public override Vector2 ScreenSpaceSelectionPoint => starHighlight.ScreenSpaceDrawQuad.Centre;
+        public override Vector2 ScreenSpaceSelectionPoint => SelectionQuad.Centre;
 
         public override bool ReceivePositionalInputAt(Vector2 screenSpacePos)
-            => starHighlight.ReceivePositionalInputAt(screenSpacePos) || bodyHighlight.ReceivePositionalInputAt(screenSpacePos);
+            => starHighlights.Any(s => s.ReceivePositionalInputAt(screenSpacePos)) || bodyHighlights.Any(b => b.ReceivePositionalInputAt(screenSpacePos));
 
-        public override Quad SelectionQuad => starHighlight.ScreenSpaceDrawQuad;
+        public override Quad SelectionQuad
+        {
+            get
+            {
+                RectangleF rect = starHighlights[0].ScreenSpaceDrawQuad.AABBFloat;
+                for (int i = 1; i < starHighlights.Count; ++i)
+                    rect = RectangleF.Union(rect, starHighlights[i].ScreenSpaceDrawQuad.AABBFloat);
+                return rect;
+            }
+        }
     }
 }
