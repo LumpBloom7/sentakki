a4481994a2ff7e7c624ab67b7ee3ef9247542507
Derrick Timmermans <derrick.timmermans@outlook.com>
2022-11-26 12:35:39 +0100

    Inject ruleset dependencies for HitObjectComposer
    Temporary measure for now, should be fixed game-side

--- osu.Game.Rulesets.Sentakki/Edit/SentakkiHitObjectComposer.cs
+++ osu.Game.Rulesets.Sentakki/Edit/SentakkiHitObjectComposer.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
+using osu.Framework.Allocation;
 using osu.Game.Rulesets.Edit;
 using osu.Game.Rulesets.Edit.Tools;
 using osu.Game.Rulesets.Sentakki.Objects;
+using osu.Game.Rulesets.UI;
 using osu.Game.Screens.Edit.Components.TernaryButtons;
 using osu.Game.Screens.Edit.Compose.Components;
 
@@ -14,6 +16,11 @@ namespace osu.Game.Rulesets.Sentakki.Edit
             : base(ruleset)
         {
         }
+
+        private DrawableRulesetDependencies dependencies = null!;
+
+        protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent)
+            => dependencies = new DrawableRulesetDependencies(Ruleset, base.CreateChildDependencies(parent));
 
         protected override IReadOnlyList<HitObjectCompositionTool> CompositionTools => new HitObjectCompositionTool[]
         {
@@ -27,5 +34,11 @@ namespace osu.Game.Rulesets.Sentakki.Edit
         protected override IEnumerable<TernaryButton> CreateTernaryButtons() => base.CreateTernaryButtons().Skip(1);
 
         protected override ComposeBlueprintContainer CreateBlueprintContainer() => new SentakkiBlueprintContainer(this);
+
+        protected override void Dispose(bool isDisposing)
+        {
+            base.Dispose(isDisposing);
+            dependencies?.Dispose();
+        }
     }
 }
