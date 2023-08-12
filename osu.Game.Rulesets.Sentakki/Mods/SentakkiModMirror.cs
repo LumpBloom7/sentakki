3ceca2bd2f31f554e9e3f354d5151a0e2efde0f6
Derrick Timmermans <derrick.timmermans@outlook.com>
2023-08-13 01:52:13 +0200

    Use ref vars to modify slidePathParts array
    Structs are still copy on write, so modifying without ref vars only affected the local copy in the loop.

--- osu.Game.Rulesets.Sentakki/Mods/SentakkiModMirror.cs
+++ osu.Game.Rulesets.Sentakki/Mods/SentakkiModMirror.cs
@@ -56,10 +56,9 @@ namespace osu.Game.Rulesets.Sentakki.Mods
                 {
                     foreach (var slideInfo in slide.SlideInfoList)
                     {
-
                         for (int i = 0; i < slideInfo.SlidePathParts.Length; ++i)
                         {
-                            var part = slideInfo.SlidePathParts[i];
+                            ref var part = ref slideInfo.SlidePathParts[i];
                             part.EndOffset = (part.EndOffset * -1).NormalizePath();
                             part.Mirrored ^= mirrored;
                         }
