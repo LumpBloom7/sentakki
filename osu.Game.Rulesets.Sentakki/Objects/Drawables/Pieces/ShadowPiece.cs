af6ad715e44d307a8d74ab418be29b45d46fba75
Derrick Timmermans <derrick.timmermans@outlook.com>
2023-08-17 16:27:21 +0200

    Prepare shadow/glow pieces for EX glow

--- osu.Game.Rulesets.Sentakki/Objects/Drawables/Pieces/ShadowPiece.cs
+++ osu.Game.Rulesets.Sentakki/Objects/Drawables/Pieces/ShadowPiece.cs
@@ -1,12 +1,27 @@
-﻿using osu.Framework.Graphics;
+﻿using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Effects;
+using osu.Game.Rulesets.Objects.Drawables;
 using osuTK.Graphics;
 
 namespace osu.Game.Rulesets.Sentakki.Objects.Drawables.Pieces
 {
     public partial class ShadowPiece : Container
     {
+        private CircularContainer glowContainer;
+
+        private Bindable<bool> ExNoteBindable = new Bindable<bool>(true);
+        private Bindable<Color4> AccentColour = new Bindable<Color4>();
+
+        private static readonly EdgeEffectParameters shadow_parameters = new EdgeEffectParameters
+        {
+            Type = EdgeEffectType.Shadow,
+            Radius = 15,
+            Colour = Color4.Black,
+        };
+
         public ShadowPiece()
         {
             RelativeSizeAxes = Axes.Both;
@@ -14,18 +29,33 @@ namespace osu.Game.Rulesets.Sentakki.Objects.Drawables.Pieces
             Origin = Anchor.Centre;
             Padding = new MarginPadding(1);
 
-            Child = new CircularContainer
+            Child = glowContainer = new CircularContainer
             {
                 Alpha = .5f,
                 Masking = true,
                 RelativeSizeAxes = Axes.Both,
-                EdgeEffect = new EdgeEffectParameters
-                {
-                    Hollow = true,
-                    Type = EdgeEffectType.Shadow,
-                    Radius = 15,
-                    Colour = Color4.Black,
-                }
+                EdgeEffect = shadow_parameters
+            };
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(DrawableHitObject hitObject)
+        {
+            // Bind exnote
+            AccentColour.BindTo(hitObject.AccentColour);
+            AccentColour.BindValueChanged(_ => updateGlow());
+
+            ExNoteBindable.BindValueChanged(_ => updateGlow(), true);
+        }
+
+        private void updateGlow()
+        {
+            if (!ExNoteBindable.Value)
+                glowContainer.EdgeEffect = shadow_parameters;
+
+            glowContainer.EdgeEffect = shadow_parameters with
+            {
+                Colour = AccentColour.Value
             };
         }
     }
--- osu.Game.Rulesets.Sentakki/Objects/Drawables/Pieces/Slides/StarPiece.cs
+++ osu.Game.Rulesets.Sentakki/Objects/Drawables/Pieces/Slides/StarPiece.cs
@@ -1,13 +1,20 @@
 using osu.Framework.Allocation;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
+using osu.Game.Rulesets.Objects.Drawables;
+using osuTK.Graphics;
 
 namespace osu.Game.Rulesets.Sentakki.Objects.Drawables.Pieces.Slides
 {
     public partial class StarPiece : CompositeDrawable
     {
+        private Sprite glowTexture = null!;
+
+        private Bindable<bool> ExNoteBindable = new Bindable<bool>();
+
         public StarPiece()
         {
             Anchor = Anchor.Centre;
@@ -17,12 +24,27 @@ namespace osu.Game.Rulesets.Sentakki.Objects.Drawables.Pieces.Slides
         [BackgroundDependencyLoader]
         private void load(TextureStore textures)
         {
+            AddInternal(glowTexture = new Sprite
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Texture = textures.Get("starGlow"),
+                Colour = Color4.White
+            });
+
             AddInternal(new Sprite
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
-                Texture = textures.Get("star"),
+                Texture = textures.Get("starNoGlow"),
             });
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(DrawableHitObject hitObject)
+        {
+            // Bind exnote
+            ExNoteBindable.BindValueChanged(v => Colour = v.NewValue ? Color4.White : Color4.Black);
         }
     }
 }
