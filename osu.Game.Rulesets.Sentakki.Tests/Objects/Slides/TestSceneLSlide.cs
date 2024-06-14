f70b03e8c0b8a22786c60e8a9035fcef22b9a394
Derrick Timmermans <derrick.timmermans@outlook.com>
2024-06-14 12:48:10 +0200

    Remove L-slides

--- osu.Game.Rulesets.Sentakki.Tests/Objects/Slides/TestSceneLSlide.cs
+++
@@ -1,9 +0,0 @@
-using osu.Game.Rulesets.Sentakki.Objects;
-
-namespace osu.Game.Rulesets.Sentakki.Tests.Objects.Slides
-{
-    public partial class TestSceneLSlide : TestSceneSlide
-    {
-        protected override SlidePaths.PathShapes PathShape => SlidePaths.PathShapes.L;
-    }
-}
