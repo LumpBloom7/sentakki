0f656b6ac762b8c97584702c648cfe9a4cf64d89
Derrick Timmermans <derrick.timmermans@outlook.com>
2024-09-08 19:33:09 +0200

    Criticals now have a base score of 305
    Similar to mania

--- osu.Game.Rulesets.Sentakki/Scoring/SentakkiScoreProcessor.cs
+++ osu.Game.Rulesets.Sentakki/Scoring/SentakkiScoreProcessor.cs
@@ -15,5 +15,13 @@ namespace osu.Game.Rulesets.Sentakki.Scoring
                    + (990000 * Math.Pow(Accuracy.Value, 2 + (2 * Accuracy.Value)) * accuracyProgress)
                    + bonusPortion;
         }
+
+        public override int GetBaseScoreForResult(HitResult result)
+        {
+            if (result == HitResult.Perfect)
+                return 305;
+
+            return base.GetBaseScoreForResult(result);
+        }
     }
 }
