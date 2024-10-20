a06a92bc170e5242ba7d3dd68eeb43bf2c2d2d24
Derrick Timmermans <derrick.timmermans@outlook.com>
2024-10-20 12:47:14 +0200

    Adjust combo ratio to match mania

--- osu.Game.Rulesets.Sentakki/Scoring/SentakkiScoreProcessor.cs
+++ osu.Game.Rulesets.Sentakki/Scoring/SentakkiScoreProcessor.cs
@@ -14,8 +14,8 @@ namespace osu.Game.Rulesets.Sentakki.Scoring
 
         protected override double ComputeTotalScore(double comboProgress, double accuracyProgress, double bonusPortion)
         {
-            return (10000 * comboProgress)
-                + (990000 * Math.Pow(Accuracy.Value, 2 + (2 * Accuracy.Value)) * accuracyProgress)
+            return (150000 * comboProgress)
+                + (850000 * Math.Pow(Accuracy.Value, 2 + (2 * Accuracy.Value)) * accuracyProgress)
                 + bonusPortion;
         }
 
