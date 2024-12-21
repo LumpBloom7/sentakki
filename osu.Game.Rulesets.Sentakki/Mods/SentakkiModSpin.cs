9f8ae452682bdbca49301a81a2521ff888f2d5a4
Derrick Timmermans <derrick.timmermans@outlook.com>
2024-12-21 19:25:50 +0800

    Show extended info for Spin mod

--- osu.Game.Rulesets.Sentakki/Mods/SentakkiModSpin.cs
+++ osu.Game.Rulesets.Sentakki/Mods/SentakkiModSpin.cs
@@ -1,7 +1,10 @@
-﻿using osu.Framework.Bindables;
+﻿using System;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Localisation;
 using osu.Game.Configuration;
+using osu.Game.Graphics.UserInterface;
+using osu.Game.Overlays.Settings;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Sentakki.Localisation.Mods;
 using osu.Game.Rulesets.Sentakki.UI;
@@ -9,10 +12,12 @@ using osu.Game.Rulesets.UI;
 
 namespace osu.Game.Rulesets.Sentakki.Mods
 {
-    public class SentakkiModSpin : Mod, IUpdatableByPlayfield
+    public partial class SentakkiModSpin : Mod, IUpdatableByPlayfield
     {
         public override string Name => "Spin";
         public override LocalisableString Description => SentakkiModSpinStrings.ModDescription;
+
+        public override string ExtendedIconInformation => SentakkiModSpinStrings.RevolutionDurationTooltip(seconds: RevolutionDuration.Value).ToString();
         public override string Acronym => "S";
 
         public override IconUsage? Icon => FontAwesome.Solid.RedoAlt;
@@ -20,7 +25,11 @@ namespace osu.Game.Rulesets.Sentakki.Mods
 
         public override double ScoreMultiplier => 1.00;
 
-        [SettingSource(typeof(SentakkiModSpinStrings), nameof(SentakkiModSpinStrings.RevolutionDuration), nameof(SentakkiModSpinStrings.RevolutionDurationDescription))]
+        [SettingSource(
+            typeof(SentakkiModSpinStrings),
+            nameof(SentakkiModSpinStrings.RevolutionDuration),
+            nameof(SentakkiModSpinStrings.RevolutionDurationDescription),
+            SettingControlType = typeof(SettingsSlider<int, RevolutionDurationSlider>))]
         public BindableNumber<int> RevolutionDuration { get; } = new BindableNumber<int>
         {
             MinValue = 3,
@@ -34,6 +43,11 @@ namespace osu.Game.Rulesets.Sentakki.Mods
             // We only rotate the main playfield
             if (playfield is SentakkiPlayfield)
                 playfield.Rotation = (float)(playfield.Time.Current / (RevolutionDuration.Value * 1000)) * 360f;
+        }
+
+        public partial class RevolutionDurationSlider : RoundedSliderBar<int>
+        {
+            public override LocalisableString TooltipText => SentakkiModSpinStrings.RevolutionDurationTooltip(Current.Value);
         }
     }
 }
