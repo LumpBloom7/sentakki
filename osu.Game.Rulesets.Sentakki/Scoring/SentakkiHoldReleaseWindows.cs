e7c8038cb2dd592927dd5f2436be27f31a8312ce
Derrick Timmermans <derrick.timmermans@outlook.com>
2025-01-12 12:23:44 +0100

    Use a blend between finale and sentakki hold release window

--- osu.Game.Rulesets.Sentakki/Scoring/SentakkiHoldReleaseWindows.cs
+++ osu.Game.Rulesets.Sentakki/Scoring/SentakkiHoldReleaseWindows.cs
@@ -6,29 +6,29 @@ public class SentakkiHoldReleaseWindows : SentakkiHitWindows
 {
     private static readonly DifficultyRange[] default_ranges =
     {
-        SimpleDifficultyRange(HitResult.Miss, 18 * timing_unit),
-        SimpleDifficultyRange(HitResult.Ok, 18 * timing_unit),
-        SimpleDifficultyRange(HitResult.Good, 12 * timing_unit),
+        SimpleDifficultyRange(HitResult.Miss, 12 * timing_unit),
+        SimpleDifficultyRange(HitResult.Ok, 12 * timing_unit),
+        SimpleDifficultyRange(HitResult.Good, 9 * timing_unit),
         SimpleDifficultyRange(HitResult.Great, 6 * timing_unit),
-        SimpleDifficultyRange(HitResult.Perfect, 2 * timing_unit),
+        SimpleDifficultyRange(HitResult.Perfect, 3 * timing_unit),
     };
 
     private static readonly DifficultyRange[] maji_ranges =
     {
-        SimpleDifficultyRange(HitResult.Miss, 12 * timing_unit),
-        SimpleDifficultyRange(HitResult.Ok, 12 * timing_unit),
+        SimpleDifficultyRange(HitResult.Miss, 9 * timing_unit),
+        SimpleDifficultyRange(HitResult.Ok, 9 * timing_unit),
         SimpleDifficultyRange(HitResult.Good, 6 * timing_unit),
-        SimpleDifficultyRange(HitResult.Great, 4 * timing_unit),
-        SimpleDifficultyRange(HitResult.Perfect, 2 * timing_unit),
+        SimpleDifficultyRange(HitResult.Great, 3 * timing_unit),
+        SimpleDifficultyRange(HitResult.Perfect, 1 * timing_unit),
     };
 
     private static readonly DifficultyRange[] gachi_ranges =
     {
-        SimpleDifficultyRange(HitResult.Miss, 12 * timing_unit),
-        SimpleDifficultyRange(HitResult.Ok, 12 * timing_unit),
+        SimpleDifficultyRange(HitResult.Miss, 9 * timing_unit),
+        SimpleDifficultyRange(HitResult.Ok, 9 * timing_unit),
         SimpleDifficultyRange(HitResult.Good, 6 * timing_unit),
-        SimpleDifficultyRange(HitResult.Great, 2 * timing_unit),
-        SimpleDifficultyRange(HitResult.Perfect, 2 * timing_unit),
+        SimpleDifficultyRange(HitResult.Great, 3 * timing_unit),
+        SimpleDifficultyRange(HitResult.Perfect, 1 * timing_unit),
     };
 
     protected override DifficultyRange[] GetDefaultRanges() => default_ranges;
