8c74cd5f98bdfdf57ff25cea202c9927e336607f
Derrick Timmermans <derrick.timmermans@outlook.com>
2025-01-19 18:14:00 +0100

    Contain all colouring code in the mod itself
    Avoids polluting the processor with mod specific code

--- osu.Game.Rulesets.Sentakki/Beatmaps/SentakkiBeatmapProcessor.cs
+++ osu.Game.Rulesets.Sentakki/Beatmaps/SentakkiBeatmapProcessor.cs
@@ -2,19 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using osu.Game.Beatmaps;
-using osu.Game.Graphics;
-using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Sentakki.Objects;
-using osu.Game.Screens.Edit;
 using osuTK.Graphics;
 
 namespace osu.Game.Rulesets.Sentakki.Beatmaps
 {
     public class SentakkiBeatmapProcessor : BeatmapProcessor
     {
-        public NoteColouringMode ColouringMode = NoteColouringMode.DEFAULT;
-
         public new SentakkiBeatmap Beatmap => (SentakkiBeatmap)base.Beatmap;
+
+        public Action<SentakkiBeatmap>? CustomNoteColouringDelegate = null;
+
         public SentakkiBeatmapProcessor(IBeatmap beatmap)
             : base(beatmap)
         {
@@ -24,28 +22,13 @@ namespace osu.Game.Rulesets.Sentakki.Beatmaps
         {
             base.PostProcess();
 
-            switch (ColouringMode)
-            {
-                case NoteColouringMode.GC_BASED:
-                    applyGCBasedNoteColouring();
-                    break;
-                case NoteColouringMode.DIVISOR_BASED:
-                    applyDivisorBasedNoteColouring();
-                    break;
-                default:
-                    applyDefaultNoteColouring();
-                    break;
-            }
+            if (CustomNoteColouringDelegate is not null)
+                CustomNoteColouringDelegate?.Invoke(Beatmap);
+            else
+                applyDefaultNoteColouring();
         }
 
-        public enum NoteColouringMode
-        {
-            DEFAULT,
-            DIVISOR_BASED,
-            GC_BASED
-        }
-
-        public void applyDefaultNoteColouring()
+        private void applyDefaultNoteColouring()
         {
             Color4 twinColor = Color4.Gold;
             Color4 breakColor = Color4.OrangeRed;
@@ -88,89 +71,6 @@ namespace osu.Game.Rulesets.Sentakki.Beatmaps
                         slideBody.NoteColour = slideBody.DefaultNoteColour;
                 }
             }
-        }
-
-        private void applyDivisorBasedNoteColouring()
-        {
-            OsuColour colours = new OsuColour();
-            foreach (var hitObject in Beatmap.HitObjects)
-            {
-                if (hitObject is Slide slide)
-                {
-                    foreach (SentakkiHitObject nested in slide.NestedHitObjects)
-                    {
-                        double startTime = getStartTime(nested);
-                        int beatDivisor = Beatmap.ControlPointInfo.GetClosestBeatDivisor(startTime);
-
-                        nested.NoteColour = BindableBeatDivisor.GetColourFor(beatDivisor, colours);
-                    }
-                }
-
-                {
-                    double startTime = getStartTime(hitObject);
-                    int beatDivisor = Beatmap.ControlPointInfo.GetClosestBeatDivisor(startTime);
-
-                    hitObject.NoteColour = BindableBeatDivisor.GetColourFor(beatDivisor, colours);
-                }
-            }
-        }
-
-        private void applyGCBasedNoteColouring()
-        {
-            OsuColour colours = new OsuColour();
-
-            var hitobjects = Beatmap.HitObjects.Where(h => h is Slide).SelectMany(s => s.NestedHitObjects).Cast<SentakkiHitObject>().ToList();
-            hitobjects.AddRange(Beatmap.HitObjects);
-            hitobjects = [.. hitobjects.OrderBy(h => h.StartTime)];
-
-            for (int i = 0; i < hitobjects.Count; ++i)
-            {
-                double currentTime = getStartTime(hitobjects[i]);
-
-                // check the back
-                double prevDelta = double.MaxValue;
-                for (int j = i - 1; j >= 0; --j)
-                {
-                    double delta = currentTime - getStartTime(hitobjects[j]);
-
-                    if (delta >= double.Epsilon)
-                    {
-                        prevDelta = delta;
-                        break;
-                    }
-                }
-
-                // check the front
-                double frontDelta = double.MaxValue;
-                for (int j = i + 1; j < hitobjects.Count; ++j)
-                {
-                    double delta = getStartTime(hitobjects[j]) - currentTime;
-
-                    if (delta >= double.Epsilon)
-                    {
-                        frontDelta = delta;
-                        break;
-                    }
-                }
-
-                double smallestDelta = double.Min(prevDelta, frontDelta);
-
-                double beatLength = Beatmap.ControlPointInfo.TimingPointAt(currentTime).BeatLength;
-
-                smallestDelta = double.Min(smallestDelta, beatLength);
-                int divisor = (int)Math.Round(beatLength / smallestDelta);
-                var colour = BindableBeatDivisor.GetColourFor(divisor, colours);
-
-                hitobjects[i].NoteColour = colour;
-            }
-        }
-
-        private static double getStartTime(HitObject ho)
-        {
-            if (ho is SlideBody sb)
-                return sb.StartTime + sb.ShootDelay;
-
-            return ho.StartTime;
         }
     }
 }
--- osu.Game.Rulesets.Sentakki/Mods/SentakkiModSynesthesia.cs
+++ osu.Game.Rulesets.Sentakki/Mods/SentakkiModSynesthesia.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using osu.Framework.Bindables;
 using osu.Game.Beatmaps;
 using osu.Game.Configuration;
@@ -6,6 +8,7 @@ using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Objects.Drawables;
 using osu.Game.Rulesets.Sentakki.Beatmaps;
+using osu.Game.Rulesets.Sentakki.Objects;
 using osu.Game.Rulesets.Sentakki.Objects.Drawables;
 using osu.Game.Screens.Edit;
 using osuTK.Graphics;
@@ -25,7 +28,91 @@ public class SentakkiModSynesthesia : ModSynesthesia, IApplicableToBeatmapProces
         if (beatmapProcessor is not SentakkiBeatmapProcessor sbp)
             return;
 
-        sbp.ColouringMode = ColourNoteGroups.Value ? SentakkiBeatmapProcessor.NoteColouringMode.GC_BASED : SentakkiBeatmapProcessor.NoteColouringMode.DIVISOR_BASED;
+        sbp.CustomNoteColouringDelegate = ColourNoteGroups.Value ? applyGCBasedNoteColouring : applyDivisorBasedNoteColouring;
+    }
+
+
+    private void applyDivisorBasedNoteColouring(SentakkiBeatmap beatmap)
+    {
+        OsuColour colours = new OsuColour();
+        foreach (var hitObject in beatmap.HitObjects)
+        {
+            if (hitObject is Slide slide)
+            {
+                foreach (SentakkiHitObject nested in slide.NestedHitObjects)
+                {
+                    double startTime = getStartTime(nested);
+                    int beatDivisor = beatmap.ControlPointInfo.GetClosestBeatDivisor(startTime);
+
+                    nested.NoteColour = BindableBeatDivisor.GetColourFor(beatDivisor, colours);
+                }
+            }
+
+            {
+                double startTime = getStartTime(hitObject);
+                int beatDivisor = beatmap.ControlPointInfo.GetClosestBeatDivisor(startTime);
+
+                hitObject.NoteColour = BindableBeatDivisor.GetColourFor(beatDivisor, colours);
+            }
+        }
+    }
+
+    private void applyGCBasedNoteColouring(SentakkiBeatmap beatmap)
+    {
+        OsuColour colours = new OsuColour();
+
+        var hitobjects = beatmap.HitObjects.Where(h => h is Slide).SelectMany(s => s.NestedHitObjects).Cast<SentakkiHitObject>().ToList();
+        hitobjects.AddRange(beatmap.HitObjects);
+        hitobjects = [.. hitobjects.OrderBy(h => h.StartTime)];
+
+        for (int i = 0; i < hitobjects.Count; ++i)
+        {
+            double currentTime = getStartTime(hitobjects[i]);
+
+            // check the back
+            double prevDelta = double.MaxValue;
+            for (int j = i - 1; j >= 0; --j)
+            {
+                double delta = currentTime - getStartTime(hitobjects[j]);
+
+                if (delta >= double.Epsilon)
+                {
+                    prevDelta = delta;
+                    break;
+                }
+            }
+
+            // check the front
+            double frontDelta = double.MaxValue;
+            for (int j = i + 1; j < hitobjects.Count; ++j)
+            {
+                double delta = getStartTime(hitobjects[j]) - currentTime;
+
+                if (delta >= double.Epsilon)
+                {
+                    frontDelta = delta;
+                    break;
+                }
+            }
+
+            double smallestDelta = double.Min(prevDelta, frontDelta);
+
+            double beatLength = beatmap.ControlPointInfo.TimingPointAt(currentTime).BeatLength;
+
+            smallestDelta = double.Min(smallestDelta, beatLength);
+            int divisor = (int)Math.Round(beatLength / smallestDelta);
+            var colour = BindableBeatDivisor.GetColourFor(divisor, colours);
+
+            hitobjects[i].NoteColour = colour;
+        }
+    }
+
+    private static double getStartTime(HitObject ho)
+    {
+        if (ho is SlideBody sb)
+            return sb.StartTime + sb.ShootDelay;
+
+        return ho.StartTime;
     }
 }
 
