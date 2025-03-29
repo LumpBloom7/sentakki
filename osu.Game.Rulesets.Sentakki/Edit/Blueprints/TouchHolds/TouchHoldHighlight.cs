dcd2d590523c69315a3e36f14834dd120e81bb72
Derrick Timmermans <derrick.timmermans@outlook.com>
2025-03-29 12:46:55 +0100

    FIx TouchHold selection palette

--- osu.Game.Rulesets.Sentakki/Edit/Blueprints/TouchHolds/TouchHoldHighlight.cs
+++ osu.Game.Rulesets.Sentakki/Edit/Blueprints/TouchHolds/TouchHoldHighlight.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Primitives;
 using osu.Game.Rulesets.Sentakki.Objects.Drawables.Pieces.TouchHolds;
@@ -7,6 +11,15 @@ namespace osu.Game.Rulesets.Sentakki.Edit.Blueprints.TouchHolds
 {
     public partial class TouchHoldHighlight : TouchHoldBody
     {
+        [Cached]
+        private Bindable<IReadOnlyList<Color4>>? paletteBindable { get; set; } = new Bindable<IReadOnlyList<Color4>>([
+                Color4.YellowGreen,
+                Color4.YellowGreen.Darken(0.7f),
+                Color4.YellowGreen,
+                Color4.YellowGreen.Darken(0.7f),
+            ]
+        );
+
         public override Quad ScreenSpaceDrawQuad => ProgressPiece.ScreenSpaceDrawQuad;
 
         public TouchHoldHighlight()
