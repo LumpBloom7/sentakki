dc1595034d361d018d512649f581d6927b6e3d44
Ayaka-chan-24 <72512337+Ayaka-chan-24@users.noreply.github.com>
2025-06-09 15:44:27 +0900

    Update SentakkiPerformanceCalculator.cs

--- osu.Game.Rulesets.Sentakki/Difficulty/SentakkiPerformanceCalculator.cs
+++ osu.Game.Rulesets.Sentakki/Difficulty/SentakkiPerformanceCalculator.cs
@@ -15,7 +15,6 @@ namespace osu.Game.Rulesets.Sentakki.Difficulty
 {
     public class SentakkiPerformanceCalculator : PerformanceCalculator
     {
-
         public SentakkiPerformanceCalculator()
             : base(new SentakkiRuleset()) { }
 
@@ -26,9 +25,9 @@ namespace osu.Game.Rulesets.Sentakki.Difficulty
             int countMiss = score.Statistics.GetValueOrDefault(HitResult.Miss);
 
             double multiplier = 1.0f;
-            double baseValue = Math.Pow(5.0f * Math.Max(1.0f, sentakkiAttributes.StarRating / 0.0049f) - 4.0f, 2.0f) / 100000.0f;
+            double baseValue = Math.Pow((5.0f * Math.Max(1.0f, sentakkiAttributes.StarRating / 0.0049f)) - 4.0f, 2.0f) / 100000.0f;
             double value = baseValue;
-            double lengthBonus = 0.95 + 0.3 * Math.Min(1.0, sentakkiAttributes.MaxCombo / 2500.0) + (sentakkiAttributes.MaxCombo > 2500 ? Math.Log10(sentakkiAttributes.MaxCombo / 2500.0f) * 0.475f : 0.0f);
+            double lengthBonus = 0.95 + ((0.3 * Math.Min(1.0, sentakkiAttributes.MaxCombo / 2500.0)) + (sentakkiAttributes.MaxCombo > 2500 ? Math.Log10(sentakkiAttributes.MaxCombo / 2500.0f) * 0.475f : 0.0f));
             value *= lengthBonus;
             value *= Math.Pow(0.97, countMiss);
             if (sentakkiAttributes.MaxCombo > 0)
