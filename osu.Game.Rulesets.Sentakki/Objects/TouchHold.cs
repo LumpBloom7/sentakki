0fe0e58a8f9cf7758288fd12e7d4a21ff86c801c
Derrick Timmermans <derrick.timmermans@outlook.com>
2025-06-16 23:39:27 +0200

    Make TouchHold implement IHasPosition interface

--- osu.Game.Rulesets.Sentakki/Objects/TouchHold.cs
+++ osu.Game.Rulesets.Sentakki/Objects/TouchHold.cs
@@ -13,7 +13,7 @@ using osuTK.Graphics;
 
 namespace osu.Game.Rulesets.Sentakki.Objects
 {
-    public partial class TouchHold : SentakkiHitObject, IHasDuration
+    public partial class TouchHold : SentakkiHitObject, IHasDuration, IHasPosition
     {
         public double EndTime
         {
