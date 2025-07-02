863422199d14321caf19cc15d82e2eaf8ea35623
Ayaka-chan-24 <72512337+Ayaka-chan-24@users.noreply.github.com>
2025-07-02 12:17:29 +0900

    Update SentakkiPerformanceCalculator.cs
    - Add combo progress instead of strain
    (Will be removed when difficulty calculation is implemented)
    - Delete unnecessary usings

--- osu.Game.Rulesets.Sentakki/Difficulty/SentakkiPerformanceCalculator.cs
+++ osu.Game.Rulesets.Sentakki/Difficulty/SentakkiPerformanceCalculator.cs
@@ -1,14 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using osu.Framework.Platform;
 using osu.Game.Rulesets.Difficulty;
-using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Scoring;
 using osu.Game.Scoring;
-using osu.Game.Screens.Play;
-using osu.Game.Screens.Play.HUD;
-using osu.Game.Screens.Ranking.Expanded.Statistics;
 
 
 namespace osu.Game.Rulesets.Sentakki.Difficulty
@@ -24,7 +18,6 @@ namespace osu.Game.Rulesets.Sentakki.Difficulty
             double accuracy = score.Accuracy;
             int countMiss = score.Statistics.GetValueOrDefault(HitResult.Miss);
 
-            double multiplier = 1.0f;
             double baseValue = Math.Pow((5.0f * Math.Max(1.0f, sentakkiAttributes.StarRating / 0.0049f)) - 4.0f, 2.0f) / 100000.0f;
             double value = baseValue;
             double lengthBonus = 0.95 + ((0.3 * Math.Min(1.0, sentakkiAttributes.MaxCombo / 2500.0)) + (sentakkiAttributes.MaxCombo > 2500 ? Math.Log10(sentakkiAttributes.MaxCombo / 2500.0f) * 0.475f : 0.0f));
@@ -33,7 +26,11 @@ namespace osu.Game.Rulesets.Sentakki.Difficulty
             if (sentakkiAttributes.MaxCombo > 0)
                 value *= Math.Min(Math.Pow(score.MaxCombo, 0.35f) / Math.Pow(sentakkiAttributes.MaxCombo, 0.35f), 1.0f);
             value *= Math.Pow(accuracy, 5.5);
-            double totalValue = value * multiplier;
+            //Until difficulty calculation is implemented,
+            //multiply combo progress instead of strain.
+            //(I think it is strange that the first combo gets 600pp.)
+            double comboProgress = (double)sentakkiAttributes.MaxCombo / score.GetMaximumAchievableCombo();
+            double totalValue = value * comboProgress;
 
             return new SentakkiPerformanceAttributes
             {
