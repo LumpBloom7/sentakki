da76d35b3e352b8648077e97e40d067d0667d541
Derrick Timmermans <derrick.timmermans@outlook.com>
2025-11-04 18:12:12 +0100

    Implement Slide selection blueprint

--- osu.Game.Rulesets.Sentakki/Edit/SentakkiBlueprintContainer.cs
+++ osu.Game.Rulesets.Sentakki/Edit/SentakkiBlueprintContainer.cs
@@ -1,13 +1,17 @@
 using System.Collections.Generic;
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Pooling;
 using osu.Framework.Input.Events;
 using osu.Game.Rulesets.Edit;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Sentakki.Edit.Blueprints.Holds;
+using osu.Game.Rulesets.Sentakki.Edit.Blueprints.Slides;
 using osu.Game.Rulesets.Sentakki.Edit.Blueprints.Taps;
 using osu.Game.Rulesets.Sentakki.Edit.Blueprints.Touches;
 using osu.Game.Rulesets.Sentakki.Edit.Blueprints.TouchHolds;
 using osu.Game.Rulesets.Sentakki.Objects;
+using osu.Game.Rulesets.Sentakki.Objects.Drawables.Pieces.Slides;
 using osu.Game.Screens.Edit.Compose.Components;
 using osuTK;
 
@@ -15,11 +19,16 @@ namespace osu.Game.Rulesets.Sentakki.Edit;
 
 public partial class SentakkiBlueprintContainer : ComposeBlueprintContainer
 {
+    [Cached]
+    private DrawablePool<SlideChevron> chevrons { get; set; }
+
     public SentakkiBlueprintContainer(HitObjectComposer composer)
         : base(composer)
     {
         Anchor = Anchor.Centre;
         Origin = Anchor.Centre;
+
+        AddInternal(chevrons = new DrawablePool<SlideChevron>(100));
     }
 
     public override HitObjectSelectionBlueprint? CreateHitObjectBlueprintFor(HitObject hitObject)
@@ -31,6 +40,9 @@ public partial class SentakkiBlueprintContainer : ComposeBlueprintContainer
 
             case Hold hold:
                 return new HoldSelectionBlueprint(hold);
+
+            case Slide slide:
+                return new SlideSelectionBlueprint(slide);
 
             case Touch touch:
                 return new TouchSelectionBlueprint(touch);
