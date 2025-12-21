971c0ffc9b25296830435821bfcffb9de8e6d088
Derrick Timmermans <derrick.timmermans@outlook.com>
2025-12-21 21:37:25 +0100

    Improve display of information

--- osu.Game.Rulesets.Sentakki/Edit/SentakkiHitObjectInspector.cs
+++ osu.Game.Rulesets.Sentakki/Edit/SentakkiHitObjectInspector.cs
@@ -1,10 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
 using osu.Framework.Allocation;
+using osu.Framework.Extensions.TypeExtensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
+using osu.Game.Overlays;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Objects.Types;
 using osu.Game.Rulesets.Sentakki.Objects;
 using osu.Game.Rulesets.Sentakki.Objects.SlidePath;
+using osu.Game.Rulesets.Sentakki.Objects.Types;
 using osu.Game.Screens.Edit;
 using osu.Game.Screens.Edit.Compose.Components;
+using osuTK.Graphics;
 
 namespace osu.Game.Rulesets.Sentakki.Edit;
 
@@ -13,90 +23,206 @@ public partial class SentakkiHitObjectInspector : HitObjectInspector
     [Resolved]
     private EditorBeatmap editorBeatmap { get; set; } = null!;
 
+    [Resolved]
+    private OverlayColourProvider colourProvider { get; set; } = null!;
+
     protected override void AddInspectorValues(HitObject[] objects)
     {
-        base.AddInspectorValues(objects);
+        switch (objects.Length)
+        {
+            default:
+            case 0:
+                base.AddInspectorValues(objects);
+                break;
 
-        if (objects.Length != 1)
+            // This is an intentional reimplementation of the base behaviour.
+            // This is done to add the beat count to the durations.
+            case 1:
+                SentakkiHitObject selected = (SentakkiHitObject)objects.Single();
+
+                AddHeader("Type");
+                AddValue($"{selected.GetType().ReadableName()}");
+
+                addPositionInformation(selected);
+
+                addModifierInformation(selected);
+                addSlideModifiersInformation(selected);
+
+                AddHeader("Time");
+                AddValue($"{selected.StartTime:#,0.##}ms");
+
+                addDurationInformation(selected);
+
+                addSlideSegmentInformation(selected);
+                break;
+        }
+    }
+
+    private void addPositionInformation(SentakkiHitObject hitObject)
+    {
+        AddHeader("Position");
+
+        switch (hitObject)
+        {
+            case IHasPosition pos:
+                AddValue($"x:{pos.X:#,0.##}");
+                AddValue($"y:{pos.Y:#,0.##}");
+                break;
+
+            case IHasLane lane:
+                AddValue($"Lane: {lane.Lane}");
+                break;
+        }
+    }
+
+    private void addDurationInformation(SentakkiHitObject hitObject)
+    {
+        if (hitObject is not IHasDuration duration)
             return;
 
-        SentakkiHitObject hitObject = (SentakkiHitObject)objects[0];
+        double beatLength = editorBeatmap.ControlPointInfo.TimingPointAt(hitObject.StartTime).BeatLength;
+        double durationInBeats = duration.Duration / beatLength;
 
-        if (hitObject is IHasDuration duration)
+        AddHeader("Duration");
+
+        addValueWithSubvalue(
+                $"{duration.Duration:#,0.##}ms",
+                $"{durationInBeats:0.##} beats",
+                colourProvider.Content1);
+
+        if (hitObject is not Slide s)
+            return;
+
+        double waitDurationInBeats = s.SlideInfoList[0].EffectiveWaitDuration / beatLength;
+        double movementDurationInBeats = s.SlideInfoList[0].EffectiveMovementDuration / beatLength;
+
+        AddHeader("Wait duration");
+
+        addValueWithSubvalue(
+            $"{s.SlideInfoList[0].EffectiveWaitDuration:#,0.##}ms",
+            $"{waitDurationInBeats:0.##} beats",
+            colourProvider.Content1
+        );
+
+        AddHeader("Movement duration");
+        addValueWithSubvalue(
+            $"{s.SlideInfoList[0].EffectiveMovementDuration:#,0.##}ms",
+            $"{movementDurationInBeats:0.##} beats",
+            colourProvider.Content1
+        );
+    }
+
+    private void addModifierInformation(SentakkiHitObject hitObject)
+    {
+        AddHeader("Slide modifiers");
+
+        List<string> modifiers = [];
+
+        if (hitObject.Break)
+            modifiers.Add("Break");
+
+        if (hitObject.Ex)
+            modifiers.Add("Ex");
+
+        if (modifiers.Count == 0)
         {
-            double beatLength = editorBeatmap.ControlPointInfo.TimingPointAt(hitObject.StartTime).BeatLength;
-            double durationInBeats = duration.Duration / beatLength;
-            AddValue($"{durationInBeats:0.##} beats");
+            AddValue("None");
+            return;
         }
 
+        AddValue(string.Join(", ", [.. modifiers]));
+    }
+
+    private void addSlideModifiersInformation(SentakkiHitObject hitObject)
+    {
+        if (hitObject is not Slide s || s.SlideInfoList.Count != 1)
+            return;
+
+        AddHeader("Slide modifiers");
+
+        List<string> modifiers = [];
+
+        if (s.SlideInfoList[0].Break)
+            modifiers.Add("Break");
+
+        if (s.SlideInfoList[0].Ex)
+            modifiers.Add("Ex");
+
+        if (modifiers.Count == 0)
         {
-            if (hitObject is Slide s && s.SlideInfoList.Count == 1)
+            AddValue("None");
+            return;
+        }
+
+        AddValue(string.Join(", ", [.. modifiers]));
+    }
+
+    private void addSlideSegmentInformation(SentakkiHitObject hitObject)
+    {
+        if (hitObject is not Slide s)
+            return;
+
+        if (s.SlideInfoList.Count != 1)
+        {
+            AddHeader("Slide bodies");
+            AddValue($"{s.SlideInfoList.Count}");
+        }
+
+        AddHeader("Segments");
+        foreach (var segment in s.SlideInfoList[0].Segments)
+        {
+            int simpleEndOffset = segment.RelativeEndLane;
+            if (simpleEndOffset > 4)
+                simpleEndOffset -= 8;
+
+            string mirrored = "";
+
+            switch (segment.Shape)
             {
-                double beatLength = editorBeatmap.ControlPointInfo.TimingPointAt(hitObject.StartTime).BeatLength;
-                double waitDurationInBeats = s.SlideInfoList[0].EffectiveWaitDuration / beatLength;
-                double movementDurationInBeats = s.SlideInfoList[0].EffectiveMovementDuration / beatLength;
+                case PathShape.Circle:
+                    mirrored = segment.Mirrored ? "CCW" : "CW";
+                    break;
 
-                AddHeader("Wait duration");
-                AddValue($"{s.SlideInfoList[0].EffectiveWaitDuration:#,0.##}ms");
-                AddValue($"{waitDurationInBeats:0.##} beats");
-
-                AddHeader("Movement duration");
-                AddValue($"{s.SlideInfoList[0].EffectiveMovementDuration:#,0.##}ms");
-                AddValue($"{movementDurationInBeats:0.##} beats");
+                case PathShape.U:
+                case PathShape.Cup:
+                case PathShape.Thunder:
+                    mirrored = segment.Mirrored ? "M" : "";
+                    break;
             }
+
+            AddValue($"{segment.Shape}({simpleEndOffset}){mirrored}");
         }
+    }
 
-        bool isBreak = hitObject.Break;
-        bool isEX = hitObject is not TouchHold && hitObject.Ex;
+    private void addValueWithSubvalue(string value, string subvalue, Color4 colour, Color4? subvalueColour = null)
+    {
+        InspectorText.AddParagraph("");
 
-        if (isBreak || isEX)
+        var wrappingContainer = new FillFlowContainer()
         {
-            AddHeader("Modifiers");
-
-            if (isBreak)
-                AddValue("Break");
-
-            if (isEX)
-                AddValue($"EX");
-        }
-
-        {
-            if (hitObject is Slide s)
-            {
-                if (s.SlideInfoList.Count != 1)
+            AutoSizeAxes = Axes.Both,
+            Direction = FillDirection.Vertical,
+            Children = [
+                new OsuSpriteText()
                 {
-                    AddHeader("Slide bodies");
-                    AddValue($"{s.SlideInfoList.Count}");
-                }
-                else
+                    Text = value,
+                    Anchor = Anchor.TopRight,
+                    Origin = Anchor.TopRight,
+                    Font = OsuFont.Torus.With(weight: FontWeight.SemiBold),
+                    Colour = colour,
+                },
+                new OsuSpriteText()
                 {
-                    AddHeader("Segments");
-                    foreach (var segment in s.SlideInfoList[0].Segments)
-                    {
-                        int simpleEndOffset = segment.RelativeEndLane;
-                        if (simpleEndOffset > 4)
-                            simpleEndOffset -= 8;
-
-                        string mirrored = "";
-
-                        switch (segment.Shape)
-                        {
-                            case PathShape.Circle:
-                                mirrored = segment.Mirrored ? "CCW" : "CW";
-                                break;
-
-                            case PathShape.U:
-                            case PathShape.Cup:
-                            case PathShape.Thunder:
-                                mirrored = segment.Mirrored ? "M" : "";
-                                break;
-                        }
-
-                        AddValue($"{segment.Shape}({simpleEndOffset}){mirrored}");
-                    }
+                    Text = subvalue,
+                    Origin = Anchor.TopRight,
+                    Anchor = Anchor.TopRight,
+                    Margin = new MarginPadding { Bottom = -5, },
+                    Font = OsuFont.Torus.With(size: 10, weight: FontWeight.SemiBold),
+                    Colour = subvalueColour ?? colour,
                 }
-            }
-        }
+            ]
+        };
 
+        InspectorText.AddArbitraryDrawable(wrappingContainer);
     }
 }
