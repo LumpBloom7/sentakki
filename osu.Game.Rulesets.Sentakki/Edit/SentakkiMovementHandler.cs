269da18574b25f19e72cfd66d91ade400ecc8ac7
Derrick Timmermans <derrick.timmermans@outlook.com>
2025-12-24 16:39:44 +0100

    Further fix to avoid weird interaction with slightly unsnapped notes

--- osu.Game.Rulesets.Sentakki/Edit/SentakkiMovementHandler.cs
+++ osu.Game.Rulesets.Sentakki/Edit/SentakkiMovementHandler.cs
@@ -71,7 +71,7 @@ public partial class SentakkiMovementHandler : Component
 
         // In order to prevent accidentally changing the time when the intention was to rotate
         // we make sure the resulting time of the notes don't jump significantly
-        if (Math.Abs(timeOffset) > editorBeatmap.GetBeatLengthAtTime(snappedTime))
+        if (Math.Abs(timeOffset) - editorBeatmap.GetBeatLengthAtTime(snappedTime) > 1)
             timeOffset = 0;
 
         if (laneOffset == 0 && timeOffset == 0)
