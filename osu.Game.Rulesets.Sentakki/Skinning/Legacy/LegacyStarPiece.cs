63c89c6203663b9c0d72b3c8e8273e13f0a6e268
Derrick Timmermans <derrick.timmermans@outlook.com>
2026-07-18 17:32:45 +0200

    Implement LegacyStar glow/shadow

--- osu.Game.Rulesets.Sentakki/Skinning/Legacy/LegacyStarPiece.cs
+++ osu.Game.Rulesets.Sentakki/Skinning/Legacy/LegacyStarPiece.cs
@@ -1,13 +1,20 @@
 using osu.Framework.Allocation;
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
+using osu.Game.Rulesets.Objects.Drawables;
+using osu.Game.Rulesets.Sentakki.Objects.Drawables;
 using osu.Game.Skinning;
+using osuTK;
+using osuTK.Graphics;
 
 namespace osu.Game.Rulesets.Sentakki.Skinning.Legacy;
 
 public partial class LegacyStarPiece : CompositeDrawable
 {
+    private Sprite glowLayer = null!;
+
     public LegacyStarPiece()
     {
         Anchor = Anchor.Centre;
@@ -15,15 +22,36 @@ public partial class LegacyStarPiece : CompositeDrawable
         RelativeSizeAxes = Axes.Both;
     }
 
+    private IBindable<bool> exBindable = new Bindable<bool>();
+
     [BackgroundDependencyLoader]
-    private void load(ISkinSource skin)
+    private void load(DrawableHitObject drawableHitObject, ISkinSource skin)
     {
         AddRangeInternal([
+            glowLayer = new Sprite
+            {
+                RelativeSizeAxes = Axes.Both,
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Scale = new Vector2(1.3f),
+
+                Texture = skin.GetTexture("sentakki/star-glow"),
+
+            },
             new Sprite
             {
                 RelativeSizeAxes = Axes.Both,
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+
                 Texture = skin.GetTexture("sentakki/star")
             }
         ]);
+
+        if (drawableHitObject is not DrawableSentakkiHitObject dsho)
+            return;
+
+        exBindable.BindTo(dsho.ExBindable);
+        exBindable.BindValueChanged(e => glowLayer.Colour = e.NewValue ? Color4.White : Color4.Black, true);
     }
 }
