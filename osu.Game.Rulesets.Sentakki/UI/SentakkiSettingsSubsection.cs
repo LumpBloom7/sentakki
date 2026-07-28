6880d0622990cc49e1c4611068a04f9414b7d99b
Derrick Timmermans <derrick.timmermans@outlook.com>
2026-07-28 18:02:36 +0200

    Add hint to clarify that detailed judgements are only for Argon skins

--- osu.Game.Rulesets.Sentakki/UI/SentakkiSettingsSubsection.cs
+++ osu.Game.Rulesets.Sentakki/UI/SentakkiSettingsSubsection.cs
@@ -45,7 +45,10 @@ public partial class SentakkiSettingsSubsection : RulesetSettingsSubsection
             new SettingsItemV2(new FormCheckBox {
                 Caption = SentakkiSettingsSubsectionStrings.ShowDetailedJudgements,
                 Current = config.GetBindable<bool>(SentakkiRulesetSettings.DetailedJudgements),
-            }) { Keywords = ["early", "late","indicators", "timing"] },
+                HintText = "Shows EARLY/LATE indicators on Argon skins."
+            }) {
+                Keywords = ["early", "late","indicators", "timing"],
+            },
 
             new SettingsItemV2(new FormEnumDropdown<ColorOption> {
                Caption = SentakkiSettingsSubsectionStrings.RingColor,
