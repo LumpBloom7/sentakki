958f60aa9afd7155ad27898a256ca00c8a5424c9
Derrick Timmermans <derrick.timmermans@outlook.com>
2026-09-18 11:51:28 +0200

    Only remove overlapping slide tap instead of the whole slide

--- osu.Game.Rulesets.Sentakki/Edit/Blueprints/LanedPlacementBlueprint.cs
+++ osu.Game.Rulesets.Sentakki/Edit/Blueprints/LanedPlacementBlueprint.cs
@@ -1,14 +1,41 @@
+using osu.Framework.Allocation;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Sentakki.Objects;
 using osu.Game.Rulesets.Sentakki.Objects.Types;
+using osu.Game.Screens.Edit;
 
 namespace osu.Game.Rulesets.Sentakki.Edit.Blueprints;
 
 public abstract partial class LanedPlacementBlueprint<T> : SentakkiPlacementBlueprint<T>
     where T : SentakkiHitObject, IHasLane, new()
 {
+    [Resolved]
+    private SentakkiHitObjectComposer composer { get; set; } = null!;
+
+    [Resolved]
+    private EditorBeatmap editorBeatmap { get; set; } = null!;
+
     public override bool ReplacesExistingObject(HitObject existing)
         => base.ReplacesExistingObject(existing)
             && (existing is IHasLane lanedNote)
-            && lanedNote.Lane == HitObject.Lane;
+            && lanedNote.Lane == HitObject.Lane
+            && CanReplaceSlide(existing);
+
+    private bool CanReplaceSlide(HitObject existing)
+    {
+        if (existing is not Slide s)
+            return true;
+
+        if (s.TapType is Slide.TapTypeEnum.None)
+            return false;
+
+        s.TapType = Slide.TapTypeEnum.None;
+
+        composer.Playfield.Remove(s);
+        composer.Playfield.Add(s);
+
+        editorBeatmap.Update(s);
+
+        return false;
+    }
 }
